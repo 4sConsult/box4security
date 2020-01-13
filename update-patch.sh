2158f596874b9c1fd823a0c050b83e40b8bb7057
Dominik Foert <dominik.foert@am-gmbh.de>
2020-01-13 13:24:36 +0100

    Update update-patch.sh

--- update-patch.sh
+++ update-patch.sh
@@ -78,29 +78,5 @@ echo "
 
 sudo touch /var/www/kibana/ebpf/15_kibana_filter.conf
 sudo ln /var/www/kibana/ebpf/15_kibana_filter.conf /etc/logstash/conf.d/suricata/15_kibana_filter.conf 
-sudo chown logstash:www-data 1/var/www/kibana/ebpf/15_kibana_filter.conf 
-sudo chmod 0664 1/var/www/kibana/ebpf/15_kibana_filter.conf 
-
-sudo mkdir /var/www/kibana/html/update/
-sudo chown www-data:www-data /var/www/kibana/html/update/ -R
-sudo cp -r /home/amadmin/box4s/Nginx/var/www/kibana/html/* /var/www/kibana/html/
-sudo mkdir /var/www/kibana/ebpf
-touch /var/www/kibana/ebpf/bypass_filter.bpf
-sudo chown suri:www-data /var/www/kibana/ebpf/bypass_filter.bpf -R
-sudo chmod 664 /var/www/kibana/ebpf/bypass_filter.bpf
-echo "not (src host $INT_IP)" | sudo tee -a /var/www/kibana/ebpf/bypass_filter.bpf
-echo "not (dst host $INT_IP)"  | sudo tee -a  /var/www/kibana/ebpf/bypass_filter.bpf
-echo "ACHTUNG"
-echo " ist das die interne IP des Systems? Wenn nicht /etc/environment INT_IP korrekt setzen"
-echo "----"
-echo "----"
-sleep 15
-echo "not (src host 127.0.0.1)"  | sudo tee -a  /var/www/kibana/ebpf/bypass_filter.bpf
-echo "not (dst host 127.0.0.1)"  | sudo tee -a  /var/www/kibana/ebpf/bypass_filter.bpf
-echo "INSERT INTO blocks_by_bpffilter VALUES ('"$INT_IP"',0,'0.0.0.0',0,'');" | sudo -u postgres psql box4S_db
-echo "INSERT INTO blocks_by_bpffilter VALUES ('0.0.0.0',0,'"$INT_IP"',0,'');" | sudo -u postgres psql box4S_db
-echo "INSERT INTO blocks_by_bpffilter VALUES ('127.0.0.1',0,'0.0.0.0',0,'');" | sudo -u postgres psql box4S_db
-echo "INSERT INTO blocks_by_bpffilter VALUES ('0.0.0.0',0,'127.0.0.1',0,'');" | sudo -u postgres psql box4S_db
-echo " Install Dashboards"
-sudo /home/amadmin/box4s/Scripts/Elastic_Scripts/import_saved_objects.sh /home/amadmin/box4s/Kibana/Dashboard_filterUpdate090120.ndjson
-
+sudo chown logstash:www-data /var/www/kibana/ebpf/15_kibana_filter.conf 
+sudo chmod 0664 /var/www/kibana/ebpf/15_kibana_filter.conf 
