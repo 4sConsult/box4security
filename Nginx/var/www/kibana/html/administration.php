f004ad56e37297830ac207fd72729bff75fd1e99
Christoph Meyer <christoph.meyer@am-gmbh.de>
2020-03-06 18:58:38 +0100

    Rework Version Table on Administration Site

--- Nginx/var/www/kibana/html/administration.php
+++ Nginx/var/www/kibana/html/administration.php
@@ -55,20 +55,26 @@ document.getElementById("dimmer").setAttribute('class','ui inverted dimmer');
 </script>
 
 <?php
-// Logik für die Anzeige der bereits installierten und installierbaren Versionen. CurVer ist die aktuell installierte Version.
+// Get TAG Count
 exec("curl -s https://gitlab.am-gmbh.de/api/v4/projects/it-security%2Fb4s/repository/tags --header 'PRIVATE-TOKEN: p3a72xCJnChRkMCdUCD6' | python3 -c 'import sys, json; print(len(json.load(sys.stdin)))'",$tagCount);
-exec("tail /home/amadmin/box4s/BOX4s-main/VERSION",$curVer);
+// exec("curl -s https://lockedbox-bugtracker.am-gmbh.de/api/v4/projects/AM-GmbH%2Fbox4s/repository/tags --header 'PRIVATE-TOKEN: Lmp3tZkURptSjWsn7tyC' | python3 -c 'import sys, json; print(len(json.load(sys.stdin)))'",$tagCount);
+
+// CurVer ist die aktuell installierte Version.
+exec("tail /home/amadmin/VERSION",$curVer);
 for($ctr=0;$ctr<$tagCount[0];$ctr++){
+  // get TAG Names
 	exec('curl -s https://gitlab.am-gmbh.de/api/v4/projects/it-security%2Fb4s/repository/tags --header "PRIVATE-TOKEN: p3a72xCJnChRkMCdUCD6" | python3 -c "import sys, json; print(json.load(sys.stdin)['.$ctr.'][\'name\'])"',$tags[$ctr]);
+  // exec('curl -s https://lockedbox-bugtracker.am-gmbh.de/api/v4/projects/AM-GmbH%2Fbox4s/repository/tags --header "PRIVATE-TOKEN: Lmp3tZkURptSjWsn7tyC" | python3 -c "import sys, json; print(json.load(sys.stdin)['.$ctr.'][\'name\'])"',$tags[$ctr]);
 }
 ?>
 </head>
 
 
-<?php
-if(isset($_GET['update'])) { echo('<body onload="openUpdateModal()">');}
-else{ echo("<body>"); }
-?>
+<?php if (isset($_GET['update'])): ?>
+  <body onload="openUpdateModal()">
+<?php else: ?>
+  <body>
+<?php endif; ?>
 <iframe src="dynamicContent.php" name="dynamic" width="0px" height="0px" id="dynamic"> </iframe>
 
 <div class="ui modal">
@@ -83,51 +89,62 @@ else{ echo("<body>"); }
   <p id="updateStatus"><br> </p>
 </div></div></div>
 
-<div class="ui divider"></div>
-<div class="ui grid padded  ">
+<div class="ui grid padded">
 <div class="two wide column">
 <p> Verfügbare Updates </p>
 <div class="ui container">
 <table class="ui celled structured table">
   <thead>
     <tr>
-      <th with="50px">Version</th>
+      <th>Version</th>
       <th>Update</th>
     </tr>
 </thead>
 <tbody>
-
 <?php
-for($ctr=0;$ctr<$tagCount[0];$ctr++) {
-echo('<tr>');
-echo('<td class="right aligned">');
-echo($tags[$ctr][0]);
-echo('</td><td>');
-if ($ctr<$tagCount[0]){
-if ($curVer[0]==$tags[$ctr+1][0]){
-	echo('<form method="get" action="administration.php">');
-
-	echo('<input type="hidden" value="'.$tags[$ctr][0].'" name="update">');
-	echo('<button class="ui button" type="submit">Update</button>');
-	echo('</form>');
-}elseif ($curVer[0]==$tags[$ctr][0]){
-	echo('<i class="large green checkmark icon"></i>');
-}elseif ($curVer[0]> $tags[$ctr][0]){
-	echo('<i class="large green checkmark icon"></i>');
-}elseif ($curVer[0]< $tags[$ctr][0]){
-echo('<i class="large red close icon"></i>');
-}
-echo ('</td>');
-}
+$ctr = 0;
+?>
+<tr>
+  <td class="right aligned"><?= $tags[$ctr][0] ?></td>
+  <td>
+<?php if ($curVer[0] < $tags[$ctr][0]): ?>
+    <form style="margin:0;" class="ui form" method="get" action="administration.php">
+      <input type="hidden" value="<?= $tags[$ctr][0] ?>" name="update">
+      <button class="ui button" type="submit">Update</button>
+    </form>
+<?php else: ?>
+  <i class="large green checkmark icon"></i>
+<?php endif; ?>
+</td>
+</tr>
+<?php
+if(++$ctr < $tagCount) {
+  // Rest of the tags if more available
+for(;$ctr<$tagCount[0];$ctr++) {
+?>
+<tr>
+<td class="right aligned">
+  <?php if ($curVer[0] == $tags[$ctr][0]): ?>
+    <b><?= $tags[$ctr][0] ?></b>
+  <?php else: ?>
+    <?= $tags[$ctr][0] ?>
+  <?php endif; ?>
+</td>
+<td>
+<?php if ($curVer[0] >= $tags[$ctr][0]): ?>
+  <i class="large green checkmark icon"></i>
+<?php else: ?>
+  <i class="large angle double up icon"></i>
+<?php endif; ?>
+</td>
+</tr>
+<?php
+  }
 }
 ?>
-
-</tr>
-  </tbody>
+</tbody>
 </table>
 </div>
-</div>
-<div class="four wide column">
 </div>
 </div>
 </body>
