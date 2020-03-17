17ba1a321863e673a34a6511c38b00abadbc3409
Constantin Tillmann <constantin.tillmann@am-gmbh.de>
2020-03-17 11:08:15 +0000

    Score Index Installation gefixt

--- Scripts/Automation/score_calculation/install_index.sh
+++ Scripts/Automation/score_calculation/install_index.sh
@@ -3,4 +3,4 @@
 # Install the index
 echo "Install the 'scores' index"
 curl -s -H "Content-type: application/json" -X PUT http://localhost:9200/scores --data-binary @index_settings.json
-
+curl -s -H "Content-type: application/json" -X PUT http://localhost:9200/scores/_mapping --data-binary @index_mapping.json
