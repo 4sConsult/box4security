10be127e10308c86c2e2a15d35f6800e718b25fe
Constantin Tillmann <constantin.tillmann@am-gmbh.de>
2020-03-20 12:35:19 +0000

    Service erstellt, Image getaggt, hochgeladen, Update-Skript erweitert

--- update-patch.sh
+++ update-patch.sh
@@ -21,7 +21,11 @@ sudo chmod +x /usr/local/bin/docker-compose
 
 # Kopiere den neuen Service an die richtige Stelle und enable den Service
 cp /home/amadmin/box4s/System/etc/systemd/box4security.service /etc/systemd/system/box4security.service
-systemctl enable box4security
+systemctl enable box4security.service
 
-# Login bei der Docker-Registry des GitLabs
+# Login bei der Docker-Registry des GitLabs und Download der Container
 docker login docker-registry.am-gmbh.de -u deployment-token-box -p KPLm6mZJFzuA9QY9oCZC
+docker-compose -f box4security.yml pull
+
+# Start des Services
+systemctl start box4security.service
