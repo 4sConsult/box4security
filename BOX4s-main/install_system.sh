b5d88f79b7f8efa5742809fdee8052a8d70db19a
Constantin Tillmann <constantin.tillmann@am-gmbh.de>
2020-03-20 17:14:50 +0000

    Sources wieder hinzugefügt

--- BOX4s-main/install_system.sh
+++ BOX4s-main/install_system.sh
@@ -96,7 +96,10 @@ systemctl start box4security.service
 
 ############################################
 
-
+wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -
+waitForNet
+sudo apt install -y apt-transport-https
+echo "deb https://artifacts.elastic.co/packages/7.x/apt stable main" | sudo tee -a /etc/apt/sources.list.d/elastic-7.x.list
 
 
 sudo apt install -y rpm nsis alien openvas=9.0.3
