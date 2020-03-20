96abd312738b3d8a0ed406658251f2459256ec55
Christoph Meyer <christoph.meyer@4sconsult.de>
2020-03-20 17:05:48 +0100

    Render index.html

--- WebApp/routes.py
+++ WebApp/routes.py
@@ -1,6 +1,7 @@
 from WebApp import app
-from .api import BPF, BPFs, LSR, LSRs, Alert
+from .api import BPF, BPFs, LSR, LSRs, Alert, Update
 from flask_restful import Api
+from flask import render_template
 
 api = Api(app)
 
@@ -9,3 +10,8 @@ api.add_resource(BPFs, '/rules/bpf/')
 api.add_resource(LSR, '/rules/logstash/<int:rule_id>')
 api.add_resource(LSRs, '/rules/logstash/')
 api.add_resource(Alert, '/alert/<int:alert_id>')
+api.add_resource(Update, '/update/')
+
+@app.route('/')
+def index():
+    return render_template('templates/index.html')
