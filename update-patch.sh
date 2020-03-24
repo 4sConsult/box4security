c140a1ab6969a46822e7fa97125a198f7697cb18
Constantin Tillmann <constantin.tillmann@am-gmbh.de>
2020-03-24 10:18:03 +0000

    Fehlerhaften Score Index nachinstalliert

--- update-patch.sh
+++ update-patch.sh
@@ -8,3 +8,7 @@ TAG=""
 cp Nginx/var/www/kibana/html/bpf_filter.php /var/www/kibana/html/bpf_filter.php
 cp Nginx/var/www/kibana/html/filteradministration.php /var/www/kibana/html/filteradministration.php
 cp Nginx/var/www/kibana/html/kibana.php /var/www/kibana/html/kibana.php
+
+# Scores Index in vorheriger Version fehlerhaft gewesen
+cd /home/amadmin/box4s/Scripts/Automation/score_calculation/
+./install_index.sh
