02628143ea6e820dcf2471d67ce07d568ff79671
Christoph Meyer <christoph.meyer@4sconsult.de>
2020-03-26 10:54:41 +0100

    Launch Update and tail log on website

--- docker/web/source/api.py
+++ docker/web/source/api.py
@@ -5,6 +5,48 @@ import requests
 import jinja2
 import os
 import json
+import io
+
+
+# https://thispointer.com/python-get-last-n-lines-of-a-text-file-like-tail-command/
+def tail(file_name, N):
+    # Create an empty list to keep the track of last N lines
+    list_of_lines = []
+    # Open file for reading in binary mode
+    with open(file_name, 'rb') as read_obj:
+        # Move the cursor to the end of the file
+        read_obj.seek(0, os.SEEK_END)
+        # Create a buffer to keep the last read line
+        buffer = bytearray()
+        # Get the current position of pointer i.e eof
+        pointer_location = read_obj.tell()
+        # Loop till pointer reaches the top of the file
+        while pointer_location >= 0:
+            # Move the file pointer to the location pointed by pointer_location
+            read_obj.seek(pointer_location)
+            # Shift pointer location by -1
+            pointer_location = pointer_location - 1
+            # read that byte / character
+            new_byte = read_obj.read(1)
+            # If the read byte is new line character then it means one line is read
+            if new_byte == b'\n':
+                # Save the line in list of lines
+                list_of_lines.append(buffer.decode()[::-1])
+                # If the size of list reaches N, then return the reversed list
+                if len(list_of_lines) == N:
+                    return list(reversed(list_of_lines))
+                # Reinitialize the byte array to save next line
+                buffer = bytearray()
+            else:
+                # If last read character is not eol then add it in buffer
+                buffer.extend(new_byte)
+
+        # As file is read completely, if there is still data in buffer, then its first line.
+        if len(buffer) > 0:
+            list_of_lines.append(buffer.decode()[::-1])
+
+    # return the reversed list
+    return list(reversed(list_of_lines))
 
 
 def writeLSRFile():
@@ -146,19 +188,6 @@ class LSRs(Resource):
         return '', 204
 
 
-# class Update(Resource):
-#     def get(self, alert_id):
-#         return {}, 501
-#
-#     def post(self):
-#         return {}, 501
-#
-#     def put(self):
-#         return {}, 501
-#
-#     def delete(self):
-#         return {}, 501
-
 class Version(Resource):
     def get(self):
         # return currently installed version
@@ -178,6 +207,26 @@ class AvailableReleases(Resource):
             # take only relevant info
             res = [{'version': tag['name'], 'message': tag['message'], 'date': tag['commit']['created_at'], 'changelog': tag['release']['description'] if tag['release'] else ''} for tag in git]
             return res
+
+
+class LaunchUpdate(Resource):
+    def __init__(self):
+        # Register Parser and argument for endpoint
+        self.parser = reqparse.RequestParser()
+        self.parser.add_argument('target', type=str)
+        self.args = self.parser.parse_args()
+
+    def post(self):
+        # launch update.sh with target version
+        targetVersion = self.args['target']
+        return {"message": "accepted"}, 200
+
+
+class UpdateLog(Resource):
+    def get(self):
+        # Return last 15 lines of updatelog file
+        lastLines = tail('/var/log/box4s/update.log', 15)
+        return {'lines': lastLines}, 200
 
 
 class Alert(Resource):
