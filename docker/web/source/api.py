240a0c45cf161214f4e82fcb2b7ea0515a624091
Christoph Meyer <christoph.meyer@4sconsult.de>
2020-03-26 15:18:17 +0100

    Another Tail update

--- docker/web/source/api.py
+++ docker/web/source/api.py
@@ -9,32 +9,30 @@ import io
 import subprocess
 
 
-# https://stackoverflow.com/questions/136168/get-last-n-lines-of-a-file-with-python-similar-to-tail?page=1&tab=votes#tab-top
-def tail(file_name, lines):
-    total_lines_wanted = lines
-
+def tail(file_name, N):
     BLOCK_SIZE = 1024
-    f.seek(0, 2)
-    block_end_byte = f.tell()
-    lines_to_go = total_lines_wanted
-    block_number = -1
-    blocks = []   # blocks of size BLOCK_SIZE, in reverse order starting from the end of the file
-    while lines_to_go > 0 and block_end_byte > 0:
-        if (block_end_byte - BLOCK_SIZE > 0):
-            # read the last block we haven't yet read
-            f.seek(block_number * BLOCK_SIZE, 2)
-            blocks.append(f.read(BLOCK_SIZE))
-        else:
-            # file too small, start from begining
-            f.seek(0, 0)
-            # only read what was not read
-            blocks.append(f.read(block_end_byte))
-        lines_found = blocks[-1].count('\n')
-        lines_to_go -= lines_found
-        block_end_byte -= BLOCK_SIZE
-        block_number -= 1
-    all_read_text = ''.join(reversed(blocks))
-    return '\n'.join(all_read_text.splitlines()[-total_lines_wanted:])
+    with io.open(file_name, 'rb'):
+        f.seek(0, 2)
+        block_end_byte = f.tell()
+        lines_to_go = N
+        block_number = -1
+        blocks = []   # blocks of size BLOCK_SIZE, in reverse order starting from the end of the file
+        while lines_to_go > 0 and block_end_byte > 0:
+            if (block_end_byte - BLOCK_SIZE > 0):
+                # read the last block we haven't yet read
+                f.seek(block_number * BLOCK_SIZE, 2)
+                blocks.append(f.read(BLOCK_SIZE))
+            else:
+                # file too small, start from begining
+                f.seek(0, 0)
+                # only read what was not read
+                blocks.append(f.read(block_end_byte))
+            lines_found = blocks[-1].count('\n')
+            lines_to_go -= lines_found
+            block_end_byte -= BLOCK_SIZE
+            block_number -= 1
+            all_read_text = ''.join(reversed(blocks))
+        return '\n'.join(all_read_text.splitlines()[-total_lines_wanted:])
 
 
 def writeLSRFile():
