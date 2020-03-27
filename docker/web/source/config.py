10e3d01937b1532eba782c05b5961fb3ae0d26c1
Christoph Meyer <christoph.meyer@4sconsult.de>
2020-03-27 13:41:21 +0100

    SIEM Dashboards eingepflegt
    Closes #22

--- docker/web/source/config.py
+++ docker/web/source/config.py
@@ -29,7 +29,12 @@ class Dashboard():
 
 Dashboards = [
     Dashboard(name='start', url='/kibana/app/kibana#/dashboard/8d13ea50-3de1-11ea-bbd4-bb7e0278945f?_g=()&_a=(fullScreenMode:!t)', parent_id='#start'),
-    Dashboard(name='ids', url='/kibana/app/kibana#/dashboard/a7bfd050-ce1d-11e9-943f-fdbfa2556276?_g=()&_a=(fullScreenMode:!t)', parent_id='#siem'),
+    Dashboard(name='siem-overview', url='/kibana/app/kibana#/dashboard/90203990-3dd9-11ea-bbd4-bb7e0278945f?_g=()&_a=(fullScreenMode:!t)', parent_id='#siem'),
+    Dashboard(name='siem-alerts', url='/kibana/app/kibana#/dashboard/a7bfd050-ce1d-11e9-943f-fdbfa2556276?_g=()&_a=(fullScreenMode:!t)', parent_id='#siem'),
+    Dashboard(name='siem-asn', url='/kibana/app/kibana#/dashboard/9391fd20-ce4e-11e9-943f-fdbfa2556276?_g=()&_a=(fullScreenMode:!t)', parent_id='#siem'),
+    Dashboard(name='siem-http', url='/kibana/app/kibana#/dashboard/691ea410-cee1-11e9-943f-fdbfa2556276?_g=()&_a=(fullScreenMode:!t)', parent_id='#siem'),
+    Dashboard(name='siem-dns', url='/kibana/app/kibana#/dashboard/1d109db0-ceed-11e9-943f-fdbfa2556276?_g=()&_a=(fullScreenMode:!t)', parent_id='#siem'),
+    Dashboard(name='siem-proto', url='/kibana/app/kibana#/dashboard/f4ad4f80-ce38-11e9-943f-fdbfa2556276?_g=()&_a=(fullScreenMode:!t)', parent_id='#siem'),
     Dashboard(name='vuln-overview', url='/kibana/app/kibana#/dashboard/140fb900-6e82-11ea-84ea-3b1a8de87e76?_g=()&_a=(fullScreenMode:!t)', parent_id='#vuln'),
     Dashboard(name='vuln-progress', url='/kibana/app/kibana#/dashboard/f8712020-cefa-11e9-943f-fdbfa2556276?_g=()&_a=(fullScreenMode:!t)', parent_id='#vuln'),
     Dashboard(name='vuln-details', url='/kibana/app/kibana#/dashboard/39c6fc40-6e81-11ea-84ea-3b1a8de87e76?_g=()&_a=(fullScreenMode:!t)', parent_id='#vuln'),
