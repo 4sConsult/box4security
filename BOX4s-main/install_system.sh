d2648c93e31a9e3e7023d9aacedf7927ae1eea00
Christoph Meyer <christoph.meyer@4sconsult.de>
2020-03-31 13:23:51 +0200

    Install jq for cron email alerting

--- BOX4s-main/install_system.sh
+++ BOX4s-main/install_system.sh
@@ -188,7 +188,7 @@ ln -s ../general/dns_resolv.conf 21-dns_resolv.conf
 ln -s ../general/AM-special.conf 25-4s_Special.conf
 cd /home/amadmin/box4s
 waitForNet
-sudo apt install -y msmtp msmtp-mta landscape-common
+sudo apt install -y msmtp msmtp-mta landscape-common jq
 sudo mkdir /home/downloads
 cd /home/downloads
 sudo chmod -R 777 /home/downloads/*
