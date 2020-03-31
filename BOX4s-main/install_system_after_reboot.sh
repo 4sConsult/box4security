98a60084b13adc78c562f1bb110bebdca8a876ca
Constantin Tillmann <mail@constantintillmann.de>
2020-03-31 08:05:19 +0200

    Datenbank ausgelagert

--- BOX4s-main/install_system_after_reboot.sh
+++ BOX4s-main/install_system_after_reboot.sh
@@ -189,21 +189,15 @@ echo ""
 echo
 echo
 cd /home/amadmin/box4s
-systemctl is-active --quiet elasticsearch
-if [ $? -ne 0 ]
-then
-  echo
-  echo
-  echo
-  echo "Elasticsearch not running. Restarting the service and waiting for 90s before continuing!!"
-  systemctl restart elasticsearch
-  sleep 90
-fi
+
 status_code=$(curl -XGET localhost:9200/_snapshot/kibana --write-out %{http_code} --silent --output /dev/null)
 if [[ "$status_code" -ne 200 ]] ; then
 	curl -XPUT localhost:9200/_snapshot/kibana -H "Content-Type: application/json" -d '{"type":"fs", "settings":{"location":"kibana"}}'
 fi
-sudo systemctl stop kibana
+
+################################################ STOP KIBANA HERE
+#sudo systemctl stop kibana
+
 curl -XDELETE localhost:9200/.kibana*
 cd /home/amadmin/box4s/Kibana/home/amadmin
 tar -xzf kibana.tar.gz
@@ -211,14 +205,15 @@ sudo cp -r kibana /data/elasticsearch_backup/Snapshots/
 rm -r kibana
 SNAPNAME=$(curl -XGET localhost:9200/_snapshot/kibana/_all?pretty --silent | grep '"snapshot" :' | tail -n 1 | grep -Po ': "\K.*(?=")')
 curl -XPOST localhost:9200/_snapshot/kibana/$SNAPNAME/_restore
-sudo systemctl start kibana
+
+################################################ START KIBANA HERE
+#sudo systemctl start kibana
 
 echo "Hole fetchqc"
 echo
 echo
 # curl -X POST "localhost:5601/kibana/api/saved_objects/_import" -H "kbn-xsrf: true" --form file=@home/amadmin/kibana-dashboard_v1.5.0.ndjson
 # Remove Cron entry
-echo "CREATE DATABASE \"box4S_db\" OWNER postgres;" | sudo -u postgres psql
 cd /home/amadmin/box4s
 cd FetchQC
 python3 -m venv .venv
@@ -236,92 +231,7 @@ echo "Install Crontab"
 cd /home/amadmin/box4s/BOX4s-main/crontab
 su - amadmin -c "crontab ~/box4s/BOX4s-main/crontab/amadmin.crontab"
 sudo crontab root.crontab
-echo "Initialisiere Datenbanken"
-echo
-echo
-cd /tmp/
-waitForNet
-curl -O -s https://iptoasn.com/data/ip2asn-combined.tsv.gz
-gunzip -f ip2asn-combined.tsv.gz
 
-if sudo -u postgres psql -lqt | cut -d \| -f 1 | grep -qw ASN_lookup_test;then
-	echo "DROP table asn; CREATE table asn (range_start INET,range_end INET, AS_number VARCHAR(10) ,country_code VARCHAR(7),AS_description VARCHAR(250)); COPY asn FROM '/tmp/ip2asn-combined.tsv' DELIMITER E'\t';" |sudo -u postgres psql ASN_lookup_test
-else
-	echo "CREATE DATABASE \"ASN_lookup_test\" OWNER postgres;" |sudo -u postgres psql
-	echo "CREATE table asn (range_start INET,range_end INET, AS_number VARCHAR(10) ,country_code VARCHAR(7),AS_description VARCHAR(250)); COPY asn FROM '/tmp/ip2asn-combined.tsv' DELIMITER E'\t';" |sudo -u postgres psql ASN_lookup_test
-
-echo "CREATE SEQUENCE public.uniquevulns_vul_id_seq
-    INCREMENT 1
-    START 27275
-    MINVALUE 1
-    MAXVALUE 2147483647
-    CACHE 1;
-
-ALTER SEQUENCE public.uniquevulns_vul_id_seq
-    OWNER TO postgres;"  |sudo -u postgres psql box4S_db
-
-echo "CREATE TABLE public.uniquevulns
- (
-     vul_id integer NOT NULL DEFAULT nextval('uniquevulns_vul_id_seq'::regclass),
-     uniqueidentifier character varying(50) COLLATE pg_catalog."default" NOT NULL,
-     CONSTRAINT uniquevulns_pkey PRIMARY KEY (vul_id),
-     CONSTRAINT uniquevulns_uniqueidentifier_key UNIQUE (uniqueidentifier)
-
- )
- WITH (
-     OIDS = FALSE
- )
- TABLESPACE pg_default;
-
- ALTER TABLE public.uniquevulns
-     OWNER to postgres;"  |sudo -u postgres psql box4S_db
-# Filter Functionality
-sudo mkdir /var/www/kibana/ebpf -p
-sudo touch /var/www/kibana/ebpf/bypass_filter.bpf
-sudo chown suri:www-data /var/www/kibana/ebpf/bypass_filter.bpf
-sudo chmod 664 /var/www/kibana/ebpf/bypass_filter.bpf
-echo "CREATE TABLE blocks_by_bpffilter
-       (
-           src_ip inet,
-           src_port integer,
-           dst_ip inet,
-           dst_port integer,
-           proto  varchar(4)
-       )
-       WITH (
-           OIDS = FALSE
-       )
-       TABLESPACE pg_default;
-       ALTER TABLE blocks_by_bpffilter
-           OWNER to postgres;" | sudo -u postgres psql box4S_db
-echo "not (src host 127.0.0.1) and"  | sudo tee -a  /var/www/kibana/ebpf/bypass_filter.bpf
-echo "not (dst host 127.0.0.1) and"  | sudo tee -a  /var/www/kibana/ebpf/bypass_filter.bpf
-echo "INSERT INTO blocks_by_bpffilter VALUES ('127.0.0.1',0,'0.0.0.0',0,'');" | sudo -u postgres psql box4S_db
-echo "INSERT INTO blocks_by_bpffilter VALUES ('0.0.0.0',0,'127.0.0.1',0,'');" | sudo -u postgres psql box4S_db
-echo "CREATE TABLE blocks_by_logstashfilter
-             (
-                 src_ip inet,
-                 src_port integer,
-                 dst_ip inet,
-                 dst_port integer,
-                 proto  varchar(4),
-                 signature_id varchar(10),
-                 signature varchar(256)
-             )
-             WITH (
-                 OIDS = FALSE
-             )
-             TABLESPACE pg_default;
-             ALTER TABLE blocks_by_logstashfilter
-                 OWNER to postgres;" | sudo -u postgres psql box4S_db
-sudo touch /var/www/kibana/ebpf/15_kibana_filter.conf
-sudo chown logstash:www-data /var/www/kibana/ebpf/15_kibana_filter.conf
-sudo chmod 0664 /var/www/kibana/ebpf/15_kibana_filter.conf
-sudo ln -s /var/www/kibana/ebpf/15_kibana_filter.conf /etc/logstash/conf.d/suricata/15_kibana_filter.conf
-
-sudo -u postgres psql -U postgres -d postgres -c "alter user postgres with password 'zgJnwauCAsHrR6JB';"
-fi
-rm ip2asn-combined.tsv
 #Add Int IP
 echo "Initialisiere Systemvariablen"
 echo
@@ -336,13 +246,12 @@ echo KUNDE="NEWSYSTEM" | sudo tee -a /etc/default/logstash
 sed -ie "s/--allow-header-host [0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}/--allow-header-host $INT_IP/g" /etc/systemd/system/greenbone-security-assistant.service
 sudo systemctl daemon-reload
 #Ignore own INT_IP
-echo "not (src host $INT_IP) and" | sudo tee -a /var/www/kibana/ebpf/bypass_filter.bpf
-echo "not (dst host $INT_IP)"  | sudo tee -a  /var/www/kibana/ebpf/bypass_filter.bpf
-echo "INSERT INTO blocks_by_bpffilter VALUES ('"$INT_IP"',0,'0.0.0.0',0,'');" | sudo -u postgres psql box4S_db
-echo "INSERT INTO blocks_by_bpffilter VALUES ('0.0.0.0',0,'"$INT_IP"',0,'');" | sudo -u postgres psql box4S_db
+#echo "not (src host $INT_IP) and" | sudo tee -a /var/www/kibana/ebpf/bypass_filter.bpf
+#echo "not (dst host $INT_IP)"  | sudo tee -a  /var/www/kibana/ebpf/bypass_filter.bpf
+#echo "INSERT INTO blocks_by_bpffilter VALUES ('"$INT_IP"',0,'0.0.0.0',0,'');" | sudo -u postgres psql box4S_db
+#echo "INSERT INTO blocks_by_bpffilter VALUES ('0.0.0.0',0,'"$INT_IP"',0,'');" | sudo -u postgres psql box4S_db
 #Copy postgres driver
 sudo cp /etc/logstash/BOX4s/postgresql-42.2.8.jar /usr/share/logstash/logstash-core/lib/jars/
-sudo chown elasticsearch:elasticsearch /data/elasticsearch -R
 sudo chown suri:suri /data/suricata/ -R
 #Updating System with openvas and installing necessary logstash plugins
 
@@ -361,14 +270,6 @@ waitForNet
 cd /home/amadmin/box4s/Scripts/Automation/score_calculation/
 ./install_index.sh
 cd /home/amadmin/box4s
-
-
-# Import BI Dashboards
-curl -X POST "localhost:5601/kibana/api/saved_objects/_import?overwrite=true" -H "kbn-xsrf: true" --form file=@/home/amadmin/box4s/Scripts/Automation/score_calculation/BIDashboards.ndjson
-# Import / Overwrite Dashboards"
-curl -X POST "localhost:5601/kibana/api/saved_objects/_import?overwrite=true" -H "kbn-xsrf: true" --form file=@/home/amadmin/box4s/Nginx/var/www/kibana/res/NetzwerkDashboards.ndjson
-curl -X POST "localhost:5601/kibana/api/saved_objects/_import?overwrite=true" -H "kbn-xsrf: true" --form file=@/home/amadmin/box4s/Nginx/var/www/kibana/res/SIEMDashboards.ndjson
-curl -X POST "localhost:5601/kibana/api/saved_objects/_import?overwrite=true" -H "kbn-xsrf: true" --form file=@/home/amadmin/box4s/Nginx/var/www/kibana/res/SchwachstellenDashboards.ndjson
 
 sudo systemctl restart networking
 sudo systemctl enable heartbeat-elastic
---
+++ docker/db/sql/box4s.sql
@@ -0,0 +1,1 @@
+CREATE DATABASE "box4S_db" OWNER postgres;
