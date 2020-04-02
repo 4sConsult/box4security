580ce16c3061244cfc2b24cd77022ef1e88a612b
Constantin Tillmann <constantin.tillmann@am-gmbh.de>
2020-04-02 10:02:52 +0200

    Logstash aus aktuellem Startup entfernt

--- BOX4s-main/install_system_after_reboot.sh
+++ BOX4s-main/install_system_after_reboot.sh
@@ -304,14 +304,12 @@ pip3 install elasticsearch-curator --user
 echo "Starte übrige Dienste"
 sudo systemctl enable heartbeat-elastic
 sudo systemctl enable suricata
-sudo systemctl enable logstash
 sudo systemctl enable metricbeat
 sudo systemctl enable filebeat
 sudo systemctl enable openvas-scanner
 sudo systemctl enable openvas-manager
 sudo systemctl enable greenbone-security-assistant
-sudo systemctl enable logstash
-sudo systemctl start logstash metricbeat filebeat openvas-scanner openvas-manager greenbone-security-assistant heartbeat-elastic suricata
+sudo systemctl start metricbeat filebeat openvas-scanner openvas-manager greenbone-security-assistant heartbeat-elastic suricata
 
 echo "Initialisiere Schwachstellendatenbank"
 sudo openvas-feed-update --verbose --progress
