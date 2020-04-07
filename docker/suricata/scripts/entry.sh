4e49afa387f97fd2369004438e9bd7a6df317630
Constantin Tillmann <constantin.tillmann@am-gmbh.de>
2020-04-07 14:19:48 +0200

    lets try this without the file

--- docker/suricata/scripts/entry.sh
+++ docker/suricata/scripts/entry.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
+IFACE=$(sudo ip addr | cut -d ' ' -f2| tr ':' '\n' | awk NF | grep -v lo | sed -n 2p | sudo tee -a /tmp/INTERFACE)
 
-IFACE=$(cat /etc/box4s/suricata/INTERFACE)
-echo $IFACE
-
-/usr/bin/suricata -vvv -c /etc/suricata/suricata.yaml -F /var/lib/box4s/suricata_suppress.bpf -i $IFACE
+sudo /usr/bin/suricata -vvv -c /etc/suricata/suricata.yaml -F /var/lib/box4s/suricata_suppress.bpf -i $IFACE
