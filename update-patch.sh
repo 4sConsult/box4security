40a84bdd6813a300a77b35fcc47824ecf3f02609
Christoph <christoph.meyer@4sconsult.de>
2020-04-07 08:33:38 +0200

    edit memory in place

--- update-patch.sh
+++ update-patch.sh
@@ -30,10 +30,10 @@ MEM=$(grep MemTotal /proc/meminfo | awk '{print $2}')
 MEM=$(python -c "print($MEM/1024**2)")
 # Die Häfte davon soll Elasticsearch zur Verfügung stehen
 ESMEM=$(python -c "print($MEM*0.5)")
-sed "s/-Xms[[:digit:]]\+g -Xmx[[:digit:]]\+g/-Xms${ESMEM}g -Xmx${ESMEM}g/g" /home/amadmin/box4s/docker/.env.es
+sed -i "s/-Xms[[:digit:]]\+g -Xmx[[:digit:]]\+g/-Xms${ESMEM}g -Xmx${ESMEM}g/g" /home/amadmin/box4s/docker/.env.es
 # 1/4 davon für Logstash
 LSMEM=$(python -c "print($MEM*0.25)")
-sed "s/-Xms[[:digit:]]\+g -Xmx[[:digit:]]\+g/-Xms${LSMEM}g -Xmx${LSMEM}g/g" /home/amadmin/box4s/docker/.env.ls
+sed -i "s/-Xms[[:digit:]]\+g -Xmx[[:digit:]]\+g/-Xms${LSMEM}g -Xmx${LSMEM}g/g" /home/amadmin/box4s/docker/.env.ls
 
 # Download IP2Location DBs for the first time
 # IP2LOCATION Token
