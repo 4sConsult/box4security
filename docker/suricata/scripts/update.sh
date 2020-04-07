65bc18262259df8c13018323d8a94ff5f1df4032
Constantin Tillmann <constantin.tillmann@am-gmbh.de>
2020-04-07 08:11:09 +0200

    resource update file added

---
+++ docker/suricata/scripts/update.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+suricata-update update-sources
+suricata-update
+suricata-update enable-source et/open
+suricata-update enable-source oisf/trafficid
+suricata-update enable-source ptresearch/attackdetection
+suricata-update enable-source sslbl/ssl-fp-blacklist
+suricata-update enable-source etnetera/aggressive
+suricata-update enable-source tgreen/hunting
+suricata-update
