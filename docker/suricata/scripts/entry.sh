ceb34a502e44c051f54f533d011289670081c9f1
Constantin Tillmann <mail@constantintillmann.de>
2020-04-09 06:56:47 +0200

    added env for interface

--- docker/suricata/scripts/entry.sh
+++ docker/suricata/scripts/entry.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-IFACE=$(sudo ip addr | cut -d ' ' -f2| tr ':' '\n' | awk NF | grep -v lo | sed -n 2p | sudo tee -a /tmp/INTERFACE)
 
-sudo /usr/bin/suricata -vvv -c /etc/suricata/suricata.yaml -F /var/lib/box4s/suricata_suppress.bpf -i $IFACE
+sudo /usr/bin/suricata -vvv -c /etc/suricata/suricata.yaml -F /var/lib/box4s/suricata_suppress.bpf -i $SURI_INTERFACE
