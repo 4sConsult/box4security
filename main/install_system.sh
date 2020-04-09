ce3a3c3c1748f8b45c1132a58e916d5066550b50
Constantin Tillmann <mail@constantintillmann.de>
2020-04-09 10:50:18 +0200

    if we need it, apt will get it ..

--- main/install_system.sh
+++ main/install_system.sh
@@ -161,13 +161,6 @@ sudo systemctl start dnsmasq
 sudo systemctl restart dnsmasq
 sudo systemctl enable dnsmasq
 waitForNet
-sudo apt install -y mc htop zsh vim libpcre3 libpcre3-dbg libpcre3-dev \
-build-essential autoconf automake libtool libpcap-dev libnet1-dev \
-libyaml-0-2 libyaml-dev pkg-config zlib1g zlib1g-dev libcap-ng-dev \
-libcap-ng0 make libmagic-dev git-core libnetfilter-queue-dev \
-libnetfilter-queue1 libnfnetlink-dev libnfnetlink0 libluajit-5.1-dev \
-libhtp-dev libnss3-dev libnspr4-dev libjansson-dev libhyperscan-dev \
-libmaxminddb-dev rustc cargo
 
 # Prepare launch of script after reboot
 sudo bash -c 'crontab -l > /tmp/crontab.root'
