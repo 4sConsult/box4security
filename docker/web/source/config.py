9bd3f4d61c8030e19205c8a8ec215ebf6875559b
Constantin Tillmann <mail@constantintillmann.de>
2020-04-14 08:01:12 +0200

    fullscreen was missing

--- docker/web/source/config.py
+++ docker/web/source/config.py
@@ -28,18 +28,18 @@ class Dashboard():
 
 
 Dashboards = [
-    Dashboard(name='start', url='/kibana/app/kibana#/dashboard/8d13ea50-3de1-11ea-bbd4-bb7e0278945f?_g=(filters:!(),refreshInterval:(pause:!t,value:0),time:(from:now%2Fd,to:now%2Fd))', parent_id='#start'),
-    Dashboard(name='siem-overview', url='/kibana/app/kibana#/dashboard/90203990-3dd9-11ea-bbd4-bb7e0278945f?_g=(filters:!(),refreshInterval:(pause:!t,value:0),time:(from:now%2Fd,to:now%2Fd))', parent_id='#siem'),
-    Dashboard(name='siem-alerts', url='/kibana/app/kibana#/dashboard/a7bfd050-ce1d-11e9-943f-fdbfa2556276?_g=(filters:!(),refreshInterval:(pause:!t,value:0),time:(from:now%2Fd,to:now%2Fd))', parent_id='#siem'),
-    Dashboard(name='siem-asn', url='/kibana/app/kibana#/dashboard/9391fd20-ce4e-11e9-943f-fdbfa2556276?_g=(filters:!(),refreshInterval:(pause:!t,value:0),time:(from:now%2Fd,to:now%2Fd))', parent_id='#siem'),
-    Dashboard(name='siem-http', url='/kibana/app/kibana#/dashboard/691ea410-cee1-11e9-943f-fdbfa2556276?_g=(filters:!(),refreshInterval:(pause:!t,value:0),time:(from:now%2Fd,to:now%2Fd))', parent_id='#siem'),
-    Dashboard(name='siem-dns', url='/kibana/app/kibana#/dashboard/1d109db0-ceed-11e9-943f-fdbfa2556276?_g=(filters:!(),refreshInterval:(pause:!t,value:0),time:(from:now%2Fd,to:now%2Fd))', parent_id='#siem'),
-    Dashboard(name='siem-proto', url='/kibana/app/kibana#/dashboard/f4ad4f80-ce38-11e9-943f-fdbfa2556276?_g=(filters:!(),refreshInterval:(pause:!t,value:0),time:(from:now%2Fd,to:now%2Fd))', parent_id='#siem'),
-    Dashboard(name='siem-social-media', url='/kibana/app/kibana#/dashboard/bf5a8370-7031-11ea-93fd-e12e440dc7e1?_g=(filters:!(),refreshInterval:(pause:!t,value:0),time:(from:now%2Fd,to:now%2Fd))', parent_id='#siem'),
-    Dashboard(name='vuln-overview', url='/kibana/app/kibana#/dashboard/140fb900-6e82-11ea-84ea-3b1a8de87e76?_g=(filters:!(),refreshInterval:(pause:!t,value:0),time:(from:now-30d,to:now))', parent_id='#vuln'),
-    Dashboard(name='vuln-progress', url='/kibana/app/kibana#/dashboard/f8712020-cefa-11e9-943f-fdbfa2556276?_g=(filters:!(),refreshInterval:(pause:!t,value:0),time:(from:now-30d,to:now))', parent_id='#vuln'),
-    Dashboard(name='vuln-details', url='/kibana/app/kibana#/dashboard/39c6fc40-6e81-11ea-84ea-3b1a8de87e76?_g=(filters:!(),refreshInterval:(pause:!t,value:0),time:(from:now-30d,to:now))', parent_id='#vuln'),
-    Dashboard(name='network-overview', url='/kibana/app/kibana#/dashboard/dc847fd0-3dd9-11ea-bbd4-bb7e0278945f?_g=(filters:!(),refreshInterval:(pause:!t,value:0),time:(from:now-30d,to:now))', parent_id='#net'),
-    Dashboard(name='network-streams', url='/kibana/app/kibana#/dashboard/e5fbd440-ce2c-11e9-943f-fdbfa2556276?_g=(filters:!(),refreshInterval:(pause:!t,value:0),time:(from:now-30d,to:now))', parent_id='#net'),
-    Dashboard(name='network-asn', url='/kibana/app/kibana#/dashboard/c2b4c450-ce46-11e9-943f-fdbfa2556276?_g=(filters:!(),refreshInterval:(pause:!t,value:0),time:(from:now-30d,to:now))', parent_id='#net')
+    Dashboard(name='start', url='/kibana/app/kibana#/dashboard/8d13ea50-3de1-11ea-bbd4-bb7e0278945f?_g=(filters:!(),refreshInterval:(pause:!t,value:0),time:(from:now%2Fd,to:now%2Fd))&_a=(fullScreenMode:!t)', parent_id='#start'),
+    Dashboard(name='siem-overview', url='/kibana/app/kibana#/dashboard/90203990-3dd9-11ea-bbd4-bb7e0278945f?_g=(filters:!(),refreshInterval:(pause:!t,value:0),time:(from:now%2Fd,to:now%2Fd))&_a=(fullScreenMode:!t)', parent_id='#siem'),
+    Dashboard(name='siem-alerts', url='/kibana/app/kibana#/dashboard/a7bfd050-ce1d-11e9-943f-fdbfa2556276?_g=(filters:!(),refreshInterval:(pause:!t,value:0),time:(from:now%2Fd,to:now%2Fd))&_a=(fullScreenMode:!t)', parent_id='#siem'),
+    Dashboard(name='siem-asn', url='/kibana/app/kibana#/dashboard/9391fd20-ce4e-11e9-943f-fdbfa2556276?_g=(filters:!(),refreshInterval:(pause:!t,value:0),time:(from:now%2Fd,to:now%2Fd))&_a=(fullScreenMode:!t)', parent_id='#siem'),
+    Dashboard(name='siem-http', url='/kibana/app/kibana#/dashboard/691ea410-cee1-11e9-943f-fdbfa2556276?_g=(filters:!(),refreshInterval:(pause:!t,value:0),time:(from:now%2Fd,to:now%2Fd))&_a=(fullScreenMode:!t)', parent_id='#siem'),
+    Dashboard(name='siem-dns', url='/kibana/app/kibana#/dashboard/1d109db0-ceed-11e9-943f-fdbfa2556276?_g=(filters:!(),refreshInterval:(pause:!t,value:0),time:(from:now%2Fd,to:now%2Fd))&_a=(fullScreenMode:!t)', parent_id='#siem'),
+    Dashboard(name='siem-proto', url='/kibana/app/kibana#/dashboard/f4ad4f80-ce38-11e9-943f-fdbfa2556276?_g=(filters:!(),refreshInterval:(pause:!t,value:0),time:(from:now%2Fd,to:now%2Fd))&_a=(fullScreenMode:!t)', parent_id='#siem'),
+    Dashboard(name='siem-social-media', url='/kibana/app/kibana#/dashboard/bf5a8370-7031-11ea-93fd-e12e440dc7e1?_g=(filters:!(),refreshInterval:(pause:!t,value:0),time:(from:now%2Fd,to:now%2Fd))&_a=(fullScreenMode:!t)', parent_id='#siem'),
+    Dashboard(name='vuln-overview', url='/kibana/app/kibana#/dashboard/140fb900-6e82-11ea-84ea-3b1a8de87e76?_g=(filters:!(),refreshInterval:(pause:!t,value:0),time:(from:now-30d,to:now))&_a=(fullScreenMode:!t)', parent_id='#vuln'),
+    Dashboard(name='vuln-progress', url='/kibana/app/kibana#/dashboard/f8712020-cefa-11e9-943f-fdbfa2556276?_g=(filters:!(),refreshInterval:(pause:!t,value:0),time:(from:now-30d,to:now))&_a=(fullScreenMode:!t)', parent_id='#vuln'),
+    Dashboard(name='vuln-details', url='/kibana/app/kibana#/dashboard/39c6fc40-6e81-11ea-84ea-3b1a8de87e76?_g=(filters:!(),refreshInterval:(pause:!t,value:0),time:(from:now-30d,to:now))&_a=(fullScreenMode:!t)', parent_id='#vuln'),
+    Dashboard(name='network-overview', url='/kibana/app/kibana#/dashboard/dc847fd0-3dd9-11ea-bbd4-bb7e0278945f?_g=(filters:!(),refreshInterval:(pause:!t,value:0),time:(from:now-30d,to:now))&_a=(fullScreenMode:!t)', parent_id='#net'),
+    Dashboard(name='network-streams', url='/kibana/app/kibana#/dashboard/e5fbd440-ce2c-11e9-943f-fdbfa2556276?_g=(filters:!(),refreshInterval:(pause:!t,value:0),time:(from:now-30d,to:now))&_a=(fullScreenMode:!t)', parent_id='#net'),
+    Dashboard(name='network-asn', url='/kibana/app/kibana#/dashboard/c2b4c450-ce46-11e9-943f-fdbfa2556276?_g=(filters:!(),refreshInterval:(pause:!t,value:0),time:(from:now-30d,to:now))&_a=(fullScreenMode:!t)', parent_id='#net')
 ]
