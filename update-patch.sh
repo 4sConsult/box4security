3e5d58b61cecd03f49b37c7589b42fc19bc04f9a
Günther, Jan <jan.guenther@4sconsult.de>
2020-04-14 12:41:18 +0200

    Update update-patch.sh

--- update-patch.sh
+++ update-patch.sh
@@ -29,7 +29,7 @@ curl -X POST "localhost:9200/_aliases" -H 'Content-Type: application/json' -d'
 }
 '
 
-# Install updated Index Pattern
+# Install updated Dashboard and Index Pattern
 curl -s -X POST "localhost:5601/kibana/api/saved_objects/_import?overwrite=true" -H "kbn-xsrf: true" --form file=@/home/amadmin/box4s/main/dashboards/Patterns/suricata.ndjson
 
 # Stop des Services
