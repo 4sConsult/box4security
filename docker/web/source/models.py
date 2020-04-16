6c9cc2a60970c04090d51ebd9e008b0e9ff0724f
Christoph <christoph.meyer@4sconsult.de>
2020-04-16 14:58:17 +0200

    Remove nocase constraint

--- docker/web/source/models.py
+++ docker/web/source/models.py
@@ -11,23 +11,22 @@ class User(db.Model, UserMixin):
     roles: Liste der, dem User zugeorndeten Regeln
     """
 
-    __tablename__ = 'users'
+    __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True)
     active = db.Column('is_active', db.Boolean(), nullable=False, server_default='1')
 
     # User authentication information. The collation='NOCASE' is required
     # to search case insensitively when USER_IFIND_MODE is 'nocase_collation'.
-    email = db.Column(db.String(255, collation='NOCASE'), nullable=False, unique=True)
+    email = db.Column(db.String(255), nullable=False, unique=True)
     email_confirmed_at = db.Column(db.DateTime())
     username = db.Column(db.String(50), nullable=False, unique=True)
     password = db.Column(db.String(255), nullable=False, server_default='')
     # User information
-    first_name = db.Column(db.String(100, collation='NOCASE'), nullable=False, server_default='')
-    last_name = db.Column(db.String(100, collation='NOCASE'), nullable=False, server_default='')
+    first_name = db.Column(db.String(100), nullable=False, server_default='')
+    last_name = db.Column(db.String(100), nullable=False, server_default='')
 
     # Define the relationship to Role via UserRoles
-    roles = db.relationship('Role', secondary='user_roles')
-
+    # roles = db.relationship('Role', secondary='user_roles')
 
 # When in doubt, see:
 # https://rahmanfadhil.com/flask-rest-api/
