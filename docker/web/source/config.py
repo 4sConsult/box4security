7f050b79503588abab4649b2d304ffc89d8abdfd
Christoph <christoph.meyer@4sconsult.de>
2020-04-16 13:27:30 +0200

    Add Secret Key to Config

--- docker/web/source/config.py
+++ docker/web/source/config.py
@@ -6,6 +6,7 @@ class Config(object):
     SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "sqlite://")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     STATIC_FOLDER = f"{os.getenv('APP_FOLDER')}/project/static"
+    SECRET_KEY = os.getenv("SECRET_KEY")
 
     # Mail
     MAIL_SERVER = os.getenv("MAIL_SERVER")
