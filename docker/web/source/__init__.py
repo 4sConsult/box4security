06fa5261c1102104a2c6c05b89aacb6234a5a707
Christoph <christoph.meyer@4sconsult.de>
2020-04-16 14:46:25 +0200

    Add Flask-Migrate

--- docker/web/source/__init__.py
+++ docker/web/source/__init__.py
@@ -1,14 +1,15 @@
 from flask import Flask
-from flask_user import UserManager
 from source.config import Config
 from source.models import User
-from source.extensions import db, ma, mail
+from source.extensions import db, ma, mail, migrate
+from flask_user import UserManager
 
 app = Flask(__name__)
 app.config.from_object(Config)
 db.init_app(app)
 ma.init_app(app)
 mail.init_app(app)
+migrate.init_app(app, db)
 userman = UserManager(app, db, User)
 
 from . import routes  # noqa
--- docker/web/source/extensions.py
+++ docker/web/source/extensions.py
@@ -2,7 +2,9 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 from flask_mail import Mail
+from flask_migrate import Migrate
 
 db = SQLAlchemy()
 ma = Marshmallow()
 mail = Mail()
+migrate = Migrate()
