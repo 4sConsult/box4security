774de7313f8bb644abd221fca0c4769f89c789c3
Christoph <christoph.meyer@4sconsult.de>
2020-04-20 11:55:03 +0200

    Override register view so we cant register more than one user

--- docker/web/source/creator.py
+++ docker/web/source/creator.py
@@ -23,8 +23,24 @@ class Initadmin():
 
 class CreatorUserMan(UserManager):
 
-    # Override or extend the default login view method
     def login_view(self):
+        """Extend default login view.
+
+        Redirect to register_view() if no user exists.
+        """
+        if self.db_manager.db_adapter.find_objects(User):
+            return super().login_view()
+        else:
+            # First time, offer registration
+            return super().register_view()
+
+    def register_view(self):
+        """Extend default register view.
+
+        Redirect login_view() to register_view() if at least 1 user exists.
+        Does not allow registering after the first user has registered.
+        Other users must be added over the WebApp.
+        """
         if self.db_manager.db_adapter.find_objects(User):
             return super().login_view()
         else:
