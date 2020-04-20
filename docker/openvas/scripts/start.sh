299583c859ec828909ab715a4cce12075231eaa4
Constantin Tillmann <mail@constantintillmann.de>
2020-04-20 11:13:44 +0200

    still testing

--- docker/openvas/scripts/start.sh
+++ docker/openvas/scripts/start.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
 
-echo "Starting openvas ..."
-service openvas-scanner start
-service openvas-manager start
-service greenbone-security-assistant start
-
 echo "Reloading NVTs ..."
 openvasmd --rebuild
