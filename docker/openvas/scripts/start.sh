ff9808fa84a3dddbaa3298217039202a54128950
Constantin Tillmann <mail@constantintillmann.de>
2020-04-20 11:40:39 +0200

    testing

--- docker/openvas/scripts/start.sh
+++ docker/openvas/scripts/start.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
+
+while [ true ] ; do
+  echo "testing"
+  sleep 10
+done
+
 echo "Start redis"
 redis-server /etc/redis/redis-openvas.conf
 
-service openvas-manager restart && \
-service openvas-scanner restart && \
-service greenbone-security-assistant restart && \
+service openvas-manager restart
+service openvas-scanner restart
+service greenbone-security-assistant restart
 
 echo "Reloading NVTs ..."
 openvasmd --rebuild
