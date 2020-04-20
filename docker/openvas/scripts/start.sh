2350778ca546491d72dbec8efe2a3c42876be1ac
Constantin Tillmann <mail@constantintillmann.de>
2020-04-20 09:56:37 +0200

    redis with openvas config does not listen on a port

--- docker/openvas/scripts/start.sh
+++ docker/openvas/scripts/start.sh
@@ -2,14 +2,6 @@
 echo "Starting redis ..."
 redis-server /etc/redis/redis-openvas.conf
 
-echo "Testing redis status ..."
-X="$(redis-cli ping)"
-while  [ "${X}" != "PONG" ]; do
-        echo "Redis not yet ready..."
-        sleep 1
-        X="$(redis-cli ping)"
-done
-
 echo "Starting openvas ..."
 service openvas-scanner start
 service openvas-manager start
