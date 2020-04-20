71f65895cef7224cde734ba655b8b43283cc3a76
Constantin Tillmann <mail@constantintillmann.de>
2020-04-20 10:50:02 +0200

    testing

--- docker/openvas/scripts/start.sh
+++ docker/openvas/scripts/start.sh
@@ -1,13 +1,4 @@
 #!/bin/bash
-echo "Starting redis ..."
-redis-server /etc/redis/redis-openvas.conf
-
-#echo "Testing redis status ..."
-#while [ ! -f /var/run/redis-openvas/redis-server.sock ]
-#do
-#  sleep 1
-#done
-
 echo "Starting openvas ..."
 service openvas-scanner start
 service openvas-manager start
--- update-patch.sh
+++ update-patch.sh
@@ -23,8 +23,8 @@ sudo chmod -R 777 /var/lib/openvas
 sudo docker volume create --driver local --opt type=none --opt device=/var/lib/openvas/ --opt o=bind varlib_openvas
 
 # Remove old Services
-sudo systemctl stop openvas-scanner openvas-manager greenbone-security-assistant
-sudo systemctl disable openvas-scanner openvas-manager greenbone-security-assistant
+sudo systemctl stop openvas-scanner openvas-manager greenbone-security-assistant redis-server
+sudo systemctl disable openvas-scanner openvas-manager greenbone-security-assistant redis-server
 sudo apt remove -y --purge openvas
 
 
