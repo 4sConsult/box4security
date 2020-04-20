2371937c66eec2ddd63698ea384ccc11d563f2f9
Constantin Tillmann <mail@constantintillmann.de>
2020-04-20 10:29:48 +0200

    test did not succeed

--- docker/openvas/scripts/start.sh
+++ docker/openvas/scripts/start.sh
@@ -1,14 +1,6 @@
 #!/bin/bash
 echo "Starting redis ..."
-redis-server /etc/openvas/redis-b4s.conf
-
-echo "Testing redis status ..."
-X="$(redis-cli ping)"
-while  [ "${X}" != "PONG" ]; do
-        echo "Redis not yet ready..."
-        sleep 1
-        X="$(redis-cli ping)"
-done
+redis-server /etc/openvas/redis-openvas.conf
 
 echo "Starting openvas ..."
 service openvas-scanner start
