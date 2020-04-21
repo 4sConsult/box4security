525e809a603277f4742deb2ab579c09a3488e9e0
Constantin Tillmann <mail@constantintillmann.de>
2020-04-21 13:35:29 +0200

    updated update

--- docker/openvas/scripts/update.sh
+++ docker/openvas/scripts/update.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 echo "Updating OpenVAS Feed ..."
+openvas-stop
+
 greenbone-scapdata-sync --verbose
 greenbone-certdata-sync --verbose
-openvas-feed-update --verbose
 greenbone-nvt-sync --verbose
+openvas-feed-update --verbose
+
+openvas-start
 openvasmd --update --verbose
 openvasmd --rebuild
