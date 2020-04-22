5b9e2bc0a263ba76180c9159ca53d9572cc90c40
Constantin Tillmann <mail@constantintillmann.de>
2020-04-22 08:29:45 +0200

    still trying

--- docker/openvas/scripts/start.sh
+++ docker/openvas/scripts/start.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+sed -i 's/--listen=127.0.0.1/--listen=0.0.0.0/g' /etc/systemd/system/greenbone-security-assistant.service
+sed -i "s/--mport=9390/--mport=9390 --allow-header-host $INT_IP/g" /etc/systemd/system/greenbone-security-assistant.service
+sed -i 's/--listen=127.0.0.1/--listen=0.0.0.0/g' /lib/systemd/system/greenbone-security-assistant.service
+sed -i 's/GSA_ADDRESS=127.0.0.1/GSA_ADDRESS=0.0.0.0/g' /etc/default/greenbone-security-assistant
 systemctl daemon-reload
 
 echo "Starting Redis ..."
