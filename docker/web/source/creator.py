8105db051a36ce89be83a2c93156155291d0e431
Christoph <christoph.meyer@4sconsult.de>
2020-04-22 07:42:15 +0200

    Remove Initadmin Class

--- docker/web/source/creator.py
+++ docker/web/source/creator.py
@@ -4,23 +4,6 @@ from flask_user import UserManager
 from source.models import User
 
 
-class Initadmin():
-    """Offer creating an admin user if no user exists."""
-
-    def __init__(self, app, db):
-        """Initialize middleware."""
-        self.app = app
-        self.db = db
-
-    def __call__(self, environ, start_response):
-        """Handle middleware call."""
-        request = Request(environ)
-        if self.db.query('User').all():
-            return Response(environ, start_response)
-        else:
-            pass
-
-
 class CreatorUserMan(UserManager):
 
     def login_view(self):
