eec346df31663375be508c0c5311415a5e0633f5
Constantin Tillmann <mail@constantintillmann.de>
2020-04-22 08:45:33 +0200

    no need for install

--- main/install_system_after_reboot.sh
+++ main/install_system_after_reboot.sh
@@ -226,12 +226,6 @@ curl  -X POST "localhost:5601/kibana/api/saved_objects/_import?overwrite=true" -
 # Installiere Suricata Index Pattern
 curl  -X POST "localhost:5601/kibana/api/saved_objects/_import?overwrite=true" -H "kbn-xsrf: true" --form file=@/home/amadmin/box4s/main/dashboards/Patterns/suricata.ndjson
 
-echo "Starte übrige Dienste"
-sudo systemctl enable openvas-scanner
-sudo systemctl enable openvas-manager
-sudo systemctl enable greenbone-security-assistant
-sudo systemctl start openvas-scanner openvas-manager greenbone-security-assistant
-
 echo "Initialisiere Schwachstellendatenbank"
 sudo greenbone-scapdata-sync --verbose --progress
 sudo greenbone-certdata-sync --verbose --progress
@@ -239,8 +233,6 @@ sudo openvas-feed-update --verbose --progress
 sudo greenbone-nvt-sync --verbose --progress
 sudo openvasmd --update --verbose --progress
 sudo openvasmd --rebuild
-
-sudo systemctl restart greenbone-security-assistant
 
 #sudo systemctl restart networking
 echo "BOX4security installiert."
