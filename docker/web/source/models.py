611da8cc3e6f4c309e8802e7c4c2cf3b207289c5
Christoph <christoph.meyer@4sconsult.de>
2020-04-22 08:27:56 +0200

    Add description ro roles

--- docker/web/source/models.py
+++ docker/web/source/models.py
@@ -34,6 +34,7 @@ class Role(db.Model):
     __tablename__ = 'role'
     id = db.Column(db.Integer(), primary_key=True)
     name = db.Column(db.String(50), unique=True)
+    description = db.Column(db.String(255))
 
 
 class UserRole(db.Model):
