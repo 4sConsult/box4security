0d3de3aa58168dc8c774fe4473d434afa669c0eb
Constantin Tillmann <mail@constantintillmann.de>
2020-04-22 09:01:38 +0200

    service should be running

--- docker/openvas/scripts/start.sh
+++ docker/openvas/scripts/start.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-# Insert Config for scan without bruteforce to openvas
-/root/run-OpenVASinsertConf.sh
 
 sed -i 's/--listen=127.0.0.1/--listen=0.0.0.0/g' /etc/systemd/system/greenbone-security-assistant.service
 sed -i "s/--mport=9390/--mport=9390 --allow-header-host $INT_IP/g" /etc/systemd/system/greenbone-security-assistant.service
@@ -17,6 +15,9 @@ echo "Starting OpenVAS ..."
 service greenbone-security-assistant start
 service openvas-scanner start
 service openvas-manager start
+
+# Insert Config for scan without bruteforce to openvas
+/root/run-OpenVASinsertConf.sh
 
 echo "Checking setup ..."
 openvas-check-setup
