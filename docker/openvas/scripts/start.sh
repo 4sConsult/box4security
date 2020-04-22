af596d52564de3dafffabb47dc1bcd678327d303
Constantin Tillmann <mail@constantintillmann.de>
2020-04-22 07:50:03 +0200

    gsad not listening correctly

--- docker/openvas/scripts/start.sh
+++ docker/openvas/scripts/start.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
-echo "Making sure the environment fits ..."
-sed -i 's/--listen=127.0.0.1/--listen=0.0.0.0/g' /lib/systemd/system/openvas-manager.service
-sed -i 's/--listen=127.0.0.1/--listen=0.0.0.0/g' /lib/systemd/system/greenbone-security-assistant.service
-
 echo "Starting Redis ..."
 # Making sure the needed directory is available
 mkdir -p /var/run/redis-openvas/
 redis-server /etc/redis/redis-openvas.conf
 
 echo "Starting OpenVAS ..."
-openvas-start
+service greenbone-security-assistant start
+service openvas-scanner start
+service openvas-manager start
 
 echo "Checking setup ..."
 openvas-check-setup
