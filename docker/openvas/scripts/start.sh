ca2574f576a950c86e15ea8375d78616ae19bbb0
Constantin Tillmann <mail@constantintillmann.de>
2020-04-22 09:43:27 +0200

    remove service reload

--- docker/openvas/scripts/start.sh
+++ docker/openvas/scripts/start.sh
@@ -4,9 +4,6 @@ sed -i 's/--listen=127.0.0.1/--listen=0.0.0.0/g' /etc/systemd/system/greenbone-s
 sed -i "s/--mport=9390/--mport=9390 --allow-header-host $INT_IP/g" /etc/systemd/system/greenbone-security-assistant.service
 sed -i 's/--listen=127.0.0.1/--listen=0.0.0.0/g' /lib/systemd/system/greenbone-security-assistant.service
 sed -i 's/GSA_ADDRESS=127.0.0.1/GSA_ADDRESS=0.0.0.0/g' /etc/default/greenbone-security-assistant
-service greenbone-security-assistant reload
-service openvas-scanner reload
-service openvas-manager reload
 
 echo "Starting Redis ..."
 # Making sure the needed directory is available
