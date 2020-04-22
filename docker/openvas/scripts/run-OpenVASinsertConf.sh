3afbdbb786c1ae4473f7c9e84b2d83c72bb54b60
Constantin Tillmann <mail@constantintillmann.de>
2020-04-22 12:56:18 +0200

    revert change

--- docker/openvas/scripts/run-OpenVASinsertConf.sh
+++ docker/openvas/scripts/run-OpenVASinsertConf.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 python3 -m venv .venv-openvas
 source .venv-openvas/bin/activate
+pip install python-gvm
 python /root/OpenVASinsertConf.py
 deactivate
 echo "OpenVAS Config Full and Fast without Default Account Check and Bruteforce imported."
