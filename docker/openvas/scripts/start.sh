fbfc938971eb80a6189ec9578256bc240f561fe1
Constantin Tillmann <mail@constantintillmann.de>
2020-04-23 07:23:02 +0200

    change order of commands

--- docker/openvas/scripts/start.sh
+++ docker/openvas/scripts/start.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
-# Make sure everything is set up front.
-# Takes time, but its the safer way and we dont bother the image building with it.
-# Probably does not take too long, when it happened alreay and the files are saved
-# on the docker volume.
-openvas-setup
-
 sed -i 's/--listen=127.0.0.1/--listen=0.0.0.0/g' /etc/systemd/system/greenbone-security-assistant.service
 sed -i "s/--mport=9390/--mport=9390 --allow-header-host $INT_IP/g" /etc/systemd/system/greenbone-security-assistant.service
 sed -i 's/--listen=127.0.0.1/--listen=0.0.0.0/g' /lib/systemd/system/greenbone-security-assistant.service
@@ -14,6 +8,12 @@ echo "Starting Redis ..."
 # Making sure the needed directory is available
 mkdir -p /var/run/redis-openvas/
 redis-server /etc/redis/redis-openvas.conf
+
+# Make sure everything is set up front.
+# Takes time, but its the safer way and we dont bother the image building with it.
+# Probably does not take too long, when it happened alreay and the files are saved
+# on the docker volume.
+openvas-setup
 
 echo "Starting OpenVAS ..."
 service greenbone-security-assistant start
