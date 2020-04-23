8b070bcc813549afae9e31a8939f7c49b702a87b
Constantin Tillmann <mail@constantintillmann.de>
2020-04-23 12:46:38 +0200

    better overview for all the files

--- scripts/Automation/score_calculation/install_index.sh
+++ scripts/Automation/score_calculation/install_index.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+DIR=$(echo "/home/amadmin/box4s/scripts/Automation/score_calculation")
+
 # Install the index
 echo "Install the 'scores' index"
 curl -s -X DELETE http://localhost:9200/scores
-curl -s -H "Content-type: application/json" -X PUT http://localhost:9200/scores --data-binary @index_settings.json
-curl -s -H "Content-type: application/json" -X PUT http://localhost:9200/scores/_mapping --data-binary @index_mapping.json
+curl -s -H "Content-type: application/json" -X PUT http://localhost:9200/scores --data-binary @$DIR/res/index_settings.json
+curl -s -H "Content-type: application/json" -X PUT http://localhost:9200/scores/_mapping --data-binary @$DIR/res/index_mapping.json
