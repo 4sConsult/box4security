36f8f0aba28e26246c0de372aa9f7e58d720501d
Constantin Tillmann <mail@constantintillmann.de>
2020-04-23 10:01:33 +0200

    fixxxxxxxxxxxxxx

--- docker/openvas/scripts/start.sh
+++ docker/openvas/scripts/start.sh
@@ -9,8 +9,10 @@ echo "Starting Redis ..."
 mkdir -p /var/run/redis-openvas/
 redis-server /etc/redis/redis-openvas.conf
 
-/usr/sbin/openvasmd --listen=127.0.0.1 --port=9390 --database=/var/lib/openvas/mgr/tasks.db
-/usr/sbin/openvassd --unix-socket=/var/run/openvassd.sock
+#/usr/sbin/openvasmd --listen=127.0.0.1 --port=9390 --database=/var/lib/openvas/mgr/tasks.db
+#/usr/sbin/openvassd --unix-socket=/var/run/openvassd.sock
+service openvas-manager start
+service openvas-scanner start
 /usr/sbin/gsad --foreground --listen=0.0.0.0 --port=9392 --mlisten=127.0.0.1 --mport=9390 --no-redirect --allow-header-host $INT_IP
 
 # Insert Config for scan without bruteforce to openvas
