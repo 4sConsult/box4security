15d7862d1467ba208ec930ed5dddec19b04b91a5
Constantin Tillmann <mail@constantintillmann.de>
2020-04-28 09:07:39 +0200

    update fix

--- update-patch.sh
+++ update-patch.sh
@@ -16,7 +16,6 @@ cd /home/amadmin/box4s/scripts/Automation/score_calculation/
 ./install_index.sh
 cd ~/box4s/
 
-
 # Stop des Services
 echo "Stopping BOX4s Service. Please wait."
 sudo systemctl stop box4security.service
@@ -30,6 +29,14 @@ sudo mkdir -p /var/lib/openvas
 sudo chown root:root /var/lib/openvas
 sudo chmod -R 777 /var/lib/openvas
 sudo docker volume create --driver local --opt type=none --opt device=/var/lib/openvas/ --opt o=bind varlib_openvas
+
+# Update Cronjobs
+cd /home/amadmin/box4s/main/crontab
+su - amadmin -c "crontab -r"
+su - amadmin -c "crontab /home/amadmin/box4s/main/crontab/amadmin.crontab"
+sudo crontab -r
+sudo crontab root.crontab
+cd ~/box4s/
 
 # Remove old Services
 sudo systemctl stop openvas-scanner openvas-manager greenbone-security-assistant redis-server
