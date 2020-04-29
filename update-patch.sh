941c735435a071e2eee1f18c43d9df9780867f59
Constantin Tillmann <mail@constantintillmann.de>
2020-04-29 11:34:14 +0200

    after release

--- update-patch.sh
+++ update-patch.sh
@@ -8,14 +8,6 @@ set -e
 #########################
 # Updates hier einfügen #
 
-# Force remove FetchQC alembic in favor of Web App alembic
-PGPASSWORD=zgJnwauCAsHrR6JB psql -h localhost -U postgres box4S_db -c "DROP TABLE alembic_version;"
-
-# Delete old index with possibly wrong data. Lets start clean!
-cd /home/amadmin/box4s/scripts/Automation/score_calculation/
-./install_index.sh
-cd ~/box4s/
-
 # Stop des Services
 echo "Stopping BOX4s Service. Please wait."
 sudo systemctl stop box4security.service
@@ -24,25 +16,7 @@ sudo docker-compose -f /home/amadmin/box4s/docker/box4security.yml pull
 
 #########################
 
-# Create the new Docker Volume
-sudo mkdir -p /var/lib/openvas
-sudo chown root:root /var/lib/openvas
-sudo chmod -R 777 /var/lib/openvas
-sudo docker volume create --driver local --opt type=none --opt device=/var/lib/openvas/ --opt o=bind varlib_openvas
 
-# Update Cronjobs
-cd /home/amadmin/box4s/main/crontab
-su - amadmin -c "crontab -r"
-su - amadmin -c "crontab /home/amadmin/box4s/main/crontab/amadmin.crontab"
-sudo crontab -r
-sudo crontab root.crontab
-cd ~/box4s/
-
-# Remove old Services
-sudo systemctl stop openvas-scanner openvas-manager greenbone-security-assistant redis-server
-sudo systemctl disable openvas-scanner openvas-manager greenbone-security-assistant redis-server
-sudo apt remove -y --purge openvas
-sudo apt autoremove -y
 
 ########################
 
