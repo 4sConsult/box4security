77628f027d617511fe751cd2d01e5fc2c6c72927
Christoph Meyer <christoph.meyer@4sconsult.de>
2020-05-20 08:30:16 +0200

    set X-Auth-Username

--- docker/web/source/routes.py
+++ docker/web/source/routes.py
@@ -5,7 +5,7 @@ from source.models import User, Role
 from source.config import Dashboards
 import source.error
 from flask_restful import Api
-from flask import render_template, send_from_directory, request, abort, send_file
+from flask import render_template, send_from_directory, request, abort, send_file, Response
 from flask_user import login_required, current_user, roles_required
 from flask_mail import Message
 from source.forms import AddUserForm
@@ -234,7 +234,8 @@ def authenticate():
         elif re.match(r'^/(docs|wiki).*$', original_uri):
             if not set(['Super Admin', 'Wiki']).isdisjoint([a.name for a in current_user.roles]):
                 # User is Super Admin or has the Wiki role
-                return "", 200
+                resp = Response("")
+                resp.headers['X-Auth-Username'] = current_user.getName()
             else:
                 # User is not permitted to request the Wiki
                 abort(403)
