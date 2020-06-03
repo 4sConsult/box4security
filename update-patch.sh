ec6eea90561e595fa069cd2cab4d7cfca419f82c
Constantin Tillmann <mail@constantintillmann.de>
2020-06-03 12:03:26 +0200

    old images will be deleted und overy update

--- update-patch.sh
+++ update-patch.sh
@@ -5,9 +5,23 @@ TAG=""
 # Exit on every error
 set -e
 
+echo "Stopping BOX4s Service. Please wait."
+sudo systemctl stop box4security.service
+
+# Remove all images, that are on the target system on every update
+sudo docker rmi $(sudo docker images -a -q)
+
+# Making sure to be logged in with the correct account
+sudo docker login registry.gitlab.com -u deployment -p B-H-Sg97y3otYdRAjFkQ
+
+# Get the current images
 sudo docker-compose -f /home/amadmin/box4s/docker/box4security.yml pull
 
 
-# Start des Services
+###################
+# Changes here
+
+###################
+
 echo "Starting BOX4s Service. Please wait."
 sudo systemctl restart box4security.service
