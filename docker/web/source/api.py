ae6a042e03d37f98a705e0e59d928677f8345182
Christoph Meyer <christoph.meyer@4sconsult.de>
2020-06-12 15:51:10 +0200

    requests json does not like preencoded json

--- docker/web/source/api.py
+++ docker/web/source/api.py
@@ -431,7 +431,7 @@ class AlertsQuick(Resource):
             return {'key': self.args['key']}, 400
         # writeQuickAlertFile(self.args['key'])
         yaml = render_template(f"application/quick_alert_{  self.args['key'] }.yaml.j2", alert={})
-        response = requests.post(f"http://elastalert:3030/rules/quick_{  self.args['key'] }", json=json.dumps({'yaml': yaml}))
+        response = requests.post(f"http://elastalert:3030/rules/quick_{  self.args['key'] }", data=json.dumps({'yaml': yaml}))
         return response.json(), 202
 
     @roles_required(['Super Admin', 'Alerts'])
