c6472f8a6840912dbf935f50bb6b944f197a8a0b
Constantin Tillmann <mail@constantintillmann.de>
2020-06-18 08:40:08 +0200

    update preparation

--- scripts/Automation/agent-upgrade.py
+++ scripts/Automation/agent-upgrade.py
@@ -1,6 +1,8 @@
 import json
 import requests
 
+# TODO: Updatescript for wazuh agents
+# https://documentation.wazuh.com/3.12/user-manual/agents/remote-upgrading/upgrading-agent.html
 url = "http://wazuh:55000/agents/outdated?pretty"
 headers = {'content-type': 'application/json', 'Accept-Charset': 'UTF-8'}
 r = requests.get(url, headers=headers, auth=('box4s', 'wa3hz0hPW'))
