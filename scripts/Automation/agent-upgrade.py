45e5abeb5bc7c7804581bea2e65a9d7318d2b1e5
Christoph Meyer <christoph.meyer@4sconsult.de>
2020-07-01 11:58:46 +0000

    Remove Secrets from agent-upgrade.py

--- scripts/Automation/agent-upgrade.py
+++ scripts/Automation/agent-upgrade.py
@@ -1,9 +1,10 @@
 import json
 import requests
+import os
 
 # TODO: Updatescript for wazuh agents
 # https://documentation.wazuh.com/3.12/user-manual/agents/remote-upgrading/upgrading-agent.html
 url = "http://wazuh:55000/agents/outdated?pretty"
 headers = {'content-type': 'application/json', 'Accept-Charset': 'UTF-8'}
-r = requests.get(url, headers=headers, auth=('box4s', 'wa3hz0hPW'))
+r = requests.get(url, headers=headers, auth=(os.getenv('WAZUH_USER'), os.getenv('WAZUH_PASS')))
 r.json()
