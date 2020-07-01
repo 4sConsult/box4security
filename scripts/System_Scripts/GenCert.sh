b61b714a36d47178f29770a82bf307f63caee3ad
Christoph Meyer <christoph.meyer@4sconsult.de>
2020-07-01 08:07:37 +0000

    Update Cert scripts

--- scripts/System_Scripts/GenCert.sh
+++ scripts/System_Scripts/GenCert.sh
@@ -2,7 +2,7 @@
 # Creates self signed certificate in a interactive session using
 # default values from box4security-ssl.conf
 # Key is without a passphrase
-openssl req -config ../../config/ssl/box4security-ssl.conf -new -x509 -sha256 -newkey rsa:4096 -nodes -keyout ../../config/secret/box4security.key.pem -days 365 -out ../../config/ssl/box4security.cert.pem
+openssl req -config ../../config/ssl/box4security-ssl.conf -new -x509 -sha256 -newkey rsa:4096 -nodes -keyout ../../config/secrets/box4security.key.pem -days 365 -out ../../config/ssl/box4security.cert.pem
 
-chmod 600 ../../config/secret/box4security.key.pem
+chmod 600 ../../config/secrets/box4security.key.pem
 chmod 644 ../../config/ssl/box4security.cert.pem
