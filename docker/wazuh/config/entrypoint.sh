3ebdb1f19ad5bf1f4fa8eeb9e79df1a29a9cf995
Christoph Meyer <christoph.meyer@4sconsult.de>
2020-07-31 10:12:00 +0000

    exit wazuh container early, if module env is not true

--- docker/wazuh/config/entrypoint.sh
+++ docker/wazuh/config/entrypoint.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 # Wazuh Docker Copyright (C) 2020 Wazuh Inc. (License GPLv2)
 
+if [ "$BOX4s_WAZUH" != "true" ]; then
+  exit 0
+fi
+
 # It will run every .sh script located in entrypoint-scripts folder in lexicographical order
 for script in `ls /entrypoint-scripts/*.sh | sort -n`; do
   bash "$script"
