856b9daa03548c4693d11dc981a2bea710fa03f2
Christoph Meyer <christoph.meyer@4sconsult.de>
2020-08-05 08:04:41 +0000

    Dont even launch wazuh if it's not an enabled module

--- docker/wazuh/config/entrypoint.sh
+++ docker/wazuh/config/entrypoint.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 # Wazuh Docker Copyright (C) 2020 Wazuh Inc. (License GPLv2)
 
-if [ "$BOX4s_WAZUH" != "true" ]; then
-  while 1; do
-    sleep 3600
-  done
-fi
-
 # It will run every .sh script located in entrypoint-scripts folder in lexicographical order
 for script in `ls /entrypoint-scripts/*.sh | sort -n`; do
   bash "$script"
