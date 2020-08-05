318c2b9fb53aaccfc822c7a3e1bf24b04c0d9bf0
Christoph Meyer <christoph.meyer@4sconsult.de>
2020-08-05 07:26:44 +0000

    Service Script: Use Wazuh Module Parameter

--- scripts/System_Scripts/box4s_service.sh
+++ scripts/System_Scripts/box4s_service.sh
@@ -2,14 +2,22 @@
 if [ $1 == "up" ]
 then
     # perform commands to set the service up
+    COMPOSE_FILES="-f /home/amadmin/box4s/docker/box4security.yml"
+    if [ $BOX4s_WAZUH == "true" ]; then
+        COMPOSE_FILES += " -f /home/amadmin/box4s/docker/wazuh/wazuh.yml"
+    fi
     # Stop and remove old container
-    /usr/local/bin/docker-compose -f /home/amadmin/box4s/docker/box4security.yml down -v
-    /usr/local/bin/docker-compose -f /home/amadmin/box4s/docker/box4security.yml rm -v
-    /usr/local/bin/docker-compose -f /home/amadmin/box4s/docker/box4security.yml up --no-color --no-build --remove-orphans
+    /usr/local/bin/docker-compose $COMPOSE_FILES down -v
+    /usr/local/bin/docker-compose $COMPOSE_FILES rm -v
+    /usr/local/bin/docker-compose $COMPOSE_FILES up --no-color --no-build --remove-orphans
 elif [ $1 == "down" ]
 then
     # perform commands to set the service down
-    /usr/local/bin/docker-compose -f /home/amadmin/box4s/docker/box4security.yml down -v
+     COMPOSE_FILES="-f /home/amadmin/box4s/docker/box4security.yml"
+    if [ $BOX4s_WAZUH == "true" ]; then
+        COMPOSE_FILES += " -f /home/amadmin/box4s/docker/wazuh/wazuh.yml"
+    fi
+    /usr/local/bin/docker-compose $COMPOSE_FILES down -v
 else
     echo "You have to submit up/down as the first parameter to the BOX4s service script."
     exit 1
