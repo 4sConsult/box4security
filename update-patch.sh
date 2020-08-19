7dfdae48e26212d3668774b81ef6f624aeb6453f
Constantin Tillmann <mail@constantintillmann.de>
2020-08-19 09:24:03 +0000

    Update update-patch.sh

--- update-patch.sh
+++ update-patch.sh
@@ -32,28 +32,6 @@ sudo chmod 777 -R /data/suricata/eve.json
 ###################
 # Changes here
 
-echo "### Activating unattended upgrades"
-printf 'APT::Periodic::Update-Package-Lists "1";\nAPT::Periodic::Unattended-Upgrade "1";' > /etc/apt/apt.conf.d/20auto-upgrades
-
-echo "### Enabling/Disabling Modules"
-sudo mkdir -p /etc/box4s/
-sudo cp /home/amadmin/box4s/config/etc/modules.conf /etc/box4s/modules.conf
-sudo chmod 444 /etc/box4s/modules.conf
-
-echo "### Updating BOX4security service for modules."
-sudo mkdir -p /usr/bin/box4s/
-sudo cp /home/amadmin/box4s/scripts/System_Scripts/box4s_service.sh /usr/bin/box4s/box4s_service.sh
-sudo chmod +x /usr/bin/box4s/box4s_service.sh
-sudo cp /home/amadmin/box4s/config/etc/systemd/box4security.service /etc/systemd/system/box4security.service
-sudo systemctl daemon-reload
-
-# Remove old quickcheck.rules and apply new
-sudo rm /var/lib/suricata/quickcheck.rules
-sudo cp /home/amadmin/box4s/docker/suricata/var_lib/quickcheck.rules /var/lib/suricata/rules/quickcheck.rules
-
-# Generate a random password for Wazuh agents
-strings /dev/urandom | grep -o '[[:alnum:]]' | head -n 14 | tr -d '\n' > /var/lib/box4s/wazuh-authd.pass
-sudo chmod 755 /var/lib/box4s/wazuh-authd.pass
 
 ###################
 
