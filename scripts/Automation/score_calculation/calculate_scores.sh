5bfc2137ba062ccc8bedd8299280dc9214350891
Christoph Meyer <christoph.meyer@4sconsult.de>
2020-08-28 09:34:20 +0000

    echo at different spot

--- scripts/Automation/score_calculation/calculate_scores.sh
+++ scripts/Automation/score_calculation/calculate_scores.sh
@@ -14,9 +14,6 @@ EPOCHTIMESTAMP=$(($(date +%s%N)/1000000))
 ALERTSCORE=$(python3 $DIR/calculate_alert_score.py)
 
 # Print the scores just in case for debugging
-echo "Alertscore: $ALERTSCORE"
-echo "Vulnscore: $VULNSCORE"
-echo "IT-Sec-Score $ITSECSCORE"
 
 # Copy the template, insert time and value and post the data to elasticsearch for ...
 # ... the alertscore
@@ -25,10 +22,12 @@ sed -i 's/%1/alert_score/g' $DIR/insert_alert_score.json
 sed -i "s/%2/$ALERTSCORE/g" $DIR/insert_alert_score.json
 sed -i "s/%3/$EPOCHTIMESTAMP/g" $DIR/insert_alert_score.json
 curl -s -H "Content-type: application/json" -X POST http://localhost:9200/scores/_doc --data-binary @$DIR/insert_alert_score.json
+echo "Alertscore: $ALERTSCORE"
 
 # ... the vulnscore
 VULNSCORE=$(python3 $DIR/calculate_vuln_score.py)
 curl -s -H "Content-type: application/json" -X POST http://localhost:9200/scores/_doc --data-binary @/tmp/vuln.scores.json
+echo "Vulnscore: $VULNSCORE"
 
 ITSECSCORE=$(echo "scale=2; ($ALERTSCORE + $VULNSCORE) / 2" | bc)
 # and the it-sec-score
@@ -37,6 +36,7 @@ sed -i 's/%1/itsec_score/g' $DIR/insert_itsec_score.json
 sed -i "s/%2/$ITSECSCORE/g" $DIR/insert_itsec_score.json
 sed -i "s/%3/$EPOCHTIMESTAMP/g" $DIR/insert_itsec_score.json
 curl -s -H "Content-type: application/json" -X POST http://localhost:9200/scores/_doc --data-binary @$DIR/insert_itsec_score.json
+echo "IT-Sec-Score $ITSECSCORE"
 
 # Delete all temp files to keep the directory clean
 rm $DIR/alert_score_result.json
