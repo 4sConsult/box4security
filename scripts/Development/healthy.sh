04d4418dc942728196748e96e4cd9967e28a876b
Christoph Meyer <christoph.meyer@4sconsult.de>
2020-09-01 14:17:58 +0000

    WIP healthy script

--- scripts/Development/healthy.sh
+++ scripts/Development/healthy.sh
@@ -1,27 +1,32 @@
 #!/bin/bash
 # Must be run as root.
 
+CONTAINERS_WITH_HEALTHCHECK=(elasticsearch nginx logstash kibana web wazuh db openvas suricata)
 
 # Define the time how long containers must stay healthy (after being healthy for the first time) to accept state.
-SAFETY_TIME=15
+SAFETY_TIME=300
 
 stay_healthy() {
-    /home/amadmin/box4s/scripts/System_Scripts/wait-for-healthy-container.sh $1
+    
     i=0
     while [ $i -lt $SAFETY_TIME ]; do
-        state=$(docker inspect -f '{{ .State.Health.Status }}' $1 2>/dev/null)
-        if [[ "${state}" != "healthy" ]]; then
-            echo "ERROR: $name went from healthy to $state."
-        fi
+        for name in "${CONTAINERS_WITH_HEALTHCHECK[@]}"
+        do
+            state=$(docker inspect -f '{{ .State.Health.Status }}' $name 2>/dev/null)
+            if [[ "${state}" != "healthy" ]]; then
+                echo "ERROR: $name went from healthy to $state."
+                exit 1
+            fi
+        done
         sleep 1
         ((i++))
     done
 }
 
-curl -sX GET --unix-socket /var/run/docker.sock http://docker/containers/json > /tmp/containers.json
-readarray -t containers < <(cat /tmp/containers.json | jq -r '.[].Names[0]' | sed 's/\///g')
-for name in "${containers[@]}"
+for name in "${CONTAINERS_WITH_HEALTHCHECK[@]}"
 do
-   (stay_healthy $name) &
+    # first let all get healthy, then check every second if one got unhealthy!
+    /home/amadmin/box4s/scripts/System_Scripts/wait-for-healthy-container.sh $name
 done
+stay_healthy
 echo "All containers staid healthy for $SAFETY_TIME seconds."
