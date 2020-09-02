16b552eca529cd3d995b962a76b0bdfd7e976f44
Jan Günther <jan.bo.guenther@gmail.com>
2020-09-02 08:49:11 +0200

    Make secrets accessible in docker container

--- docker/core4s/scripts/Automation/ASN_update.sh
+++ docker/core4s/scripts/Automation/ASN_update.sh
@@ -1,8 +1,8 @@
-source core4s/config/secrets/db.conf
+source $BASEDIR$GITDIR/config/secrets/db.conf
 cd /tmp/
 curl -O -s https://iptoasn.com/data/ip2asn-combined.tsv.gz
 gunzip -f ip2asn-combined.tsv.gz
-docker cp ip2asn-combined.tsv db:/tmp/ip2asn-combined.tsv
+sudo docker cp ip2asn-combined.tsv db:/tmp/ip2asn-combined.tsv
 if true; then
 	# assume db exists.. because we create it from docker build..
 	echo "DROP table asn; CREATE table asn (range_start INET,range_end INET, AS_number VARCHAR(10) ,country_code VARCHAR(7),AS_description VARCHAR(250)); COPY asn FROM '/tmp/ip2asn-combined.tsv' DELIMITER E'\t';" | PGPASSWORD=$POSTGRES_PASSWORD PGUSER=$POSTGRES_USER psql postgres://localhost/box4S_db
