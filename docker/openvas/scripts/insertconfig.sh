eeef2aad8aac32cb3a9eda0e70d480a9e15b2795
Christoph Meyer <christoph.meyer@4sconsult.de>
2020-09-03 08:09:30 +0000

    Fix insertconfig

--- docker/openvas/scripts/insertconfig.sh
+++ docker/openvas/scripts/insertconfig.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 CONFIG=/etc/openvas/4s-OpenVAS.xml
 
-if [ -f "$FILE" ]; then
+if [ -f "$CONFIG" ]; then
   python3 -m venv .venv-openvas
   source .venv-openvas/bin/activate
   pip install python-gvm
-  python /root/config.py
+  python3 /root/config.py
   deactivate
   echo "OpenVAS Config Full and Fast without Default Account Check and Bruteforce imported."
   rm -r .venv-openvas
-  rm CONFIG
+  rm $CONFIG
 else
   echo "Config has already been imported"
 fi
