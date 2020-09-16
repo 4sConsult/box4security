d5c7dfb8f3aa2c86f0fdbb35d9badfeb1fcf9796
Jan Günther <jan.bo.guenther@gmail.com>
2020-09-16 10:06:20 +0200

    supress output of elasticsearch index delete

--- docker/core4s/scripts/Automation/score_calculation/install_index.sh
+++ docker/core4s/scripts/Automation/score_calculation/install_index.sh
@@ -4,8 +4,7 @@ DIR=$(echo "/core4s/scripts/Automation/score_calculation")
 
 echo "Install the 'scores' index"
 # Delete an old index, which might exist, so there is no conflict
-curl -s -X DELETE http://elasticsearch:9200/scores
-
+curl -s -X DELETE http://elasticsearch:9200/scores > /dev/null
 # Create a new index called 'scores' with specific settings configured in index_settings.json.
 # Also apply a specific mapping, so everything stays the same all the time
 curl -s -H "Content-type: application/json" -X PUT http://elasticsearch:9200/scores --data-binary @$DIR/res/index_settings.json
