11b0db231725362657b628b18469883a51fa266b
Christoph Meyer <christoph.meyer@4sconsult.de>
2020-09-21 13:11:26 +0000

    insert 4sconsult configto openvas after start is completed

--- docker/openvas/scripts/start.sh
+++ docker/openvas/scripts/start.sh
@@ -3,8 +3,8 @@ echo "Setting Authentication"
 export USERNAME=$OPENVAS_USER
 export PASSWORD=$OPENVAS_PASS
 
+/start.sh
+
 echo "Inserting 4sConsult config ..."
 chmod +x /root/insertconfig.sh
 /root/insertconfig.sh
-
-/start.sh
