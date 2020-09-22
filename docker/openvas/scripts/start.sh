15696fca53b0dcb14185ec40106f3b4cf3f6895a
Christoph Meyer <christoph.meyer@4sconsult.de>
2020-09-22 08:44:40 +0000

    Modify openvas11 s.t. API is open to other containers and the config is inserted at the end of the start

--- docker/openvas/scripts/start.sh
+++ docker/openvas/scripts/start.sh
@@ -3,8 +3,13 @@ echo "Setting Authentication"
 export USERNAME=$OPENVAS_USER
 export PASSWORD=$OPENVAS_PASS
 
-/start.sh
+# Make sure the API listens to 0.0.0.0 and is thus accessible by other containers
+sed -i 's/su -c "gvmd --listen=127.0.0.1 --port=9390" gvm/su -c "gvmd --listen=0.0.0.0 --port=9390" gvm/g' /start.sh
 
-echo "Inserting 4sConsult config ..."
-chmod +x /root/insertconfig.sh
-/root/insertconfig.sh
+# Insert our config insertion before the end of start script..
+sed -i "\$i echo 'Inserting 4sConsult config ...'" /start.sh
+sed -i "\$i chmod +x /root/insertconfig.sh" /start.sh 
+sed -i "\$i /root/insertconfig.sh" /start.sh 
+
+echo "Starting OpenVAS"
+/start.sh
