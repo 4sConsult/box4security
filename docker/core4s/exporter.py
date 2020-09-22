1cfebef09224586c5fde870ea89933ddd152e1b6
Christoph Meyer <christoph.meyer@4sconsult.de>
2020-09-22 08:43:51 +0000

    exporter: read auth data from config file

--- docker/core4s/exporter.py
+++ docker/core4s/exporter.py
@@ -1,18 +1,24 @@
 #!/bin/python3
-from gvm.connctions import TLSConnection
+from gvm.connections import TLSConnection
 from gvm.protocols.gmp import Gmp 
 from gvm.transforms import EtreeTransform
 from gvm.xml import pretty_print
 import xml.etree.ElementTree as ET
 import untangle
+import configparser
+CONFIG_PATH = '/core4s/config/secrets/openvas.conf'
 
-connection = TLSConnection(hostname="openvas")
-username = "admin"
-password = "admin"
+config = configparser.ConfigParser()
+with open(CONFIG_PATH, 'r') as f:
+    config_string = '[config]\n' + f.read()
+config.read_string(config_string)
 
 
+connection = TLSConnection(hostname="openvas", port=9390)
 def exportReports():
     with Gmp(connection) as gmp:
-        gmp.authenticate(username, password)
+        gmp.authenticate(config['config']['OPENVAS_USER'], config['config']['OPENVAS_PASS'])
         reportFormat = gmp.get_report_formats()
         pretty_print(reportFormat)
+
+exportReports()
