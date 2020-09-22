68a7e67c0f1b2696a10df5e8c8c5b4892501dc0f
Christoph Meyer <christoph.meyer@4sconsult.de>
2020-09-22 14:30:36 +0000

    Query if the vuln has been processed before via sqlite3 db

--- docker/core4s/exporter.py
+++ docker/core4s/exporter.py
@@ -44,21 +44,22 @@ def handleReports(reportIds, reportFormatId):
         formattedReport = gmp.get_report(reportId, report_format_id=reportFormatId, filter="apply_overrides=0 min_qod=70", ignore_pagination=True, details=True)
         untangledReport = untangle.parse(formattedReport)
         resultId = untangledReport.get_reports_response.report['id']
-        base64CSV = untangledReport.get_reports_response.report.cdata
-        data = str(base64.b64decode(base64CSV), 'utf-8')
-        writeReport(resultId, data)
-        numWritten += 1
+        if isReportFresh(resultId):
+            base64CSV = untangledReport.get_reports_response.report.cdata
+            data = str(base64.b64decode(base64CSV), 'utf-8')
+            writeReport(resultId, data)
+            numWritten += 1
     return numWritten
 
 
 def writeReport(resultId, data):
-    if isReportFresh(resultId):
-        pass
+    dbCursor.execute('''INSERT INTO `reports`(resultId) VALUES(?)''', (resultId, ))
+    dbConn.commit()
 
 
 def isReportFresh(resultId):
-    dbCursor.execute("SELECT EXISTS(SELECT 1 FROM `reports` WHERE resultId='?')", (resultId, ))
-    return dbConn.fetchone()
+    dbCursor.execute("SELECT EXISTS(SELECT 1 FROM `reports` WHERE resultId=?)", (resultId, ))
+    return not dbCursor.fetchone()
 
 
 # Prepare TLS Connection to OpenVAS XML API
@@ -77,7 +78,7 @@ with Gmp(connection) as gmp:
     reportFormatId = getReportFormat()
     reportIds = getReportIds()
     numWritten = handleReports(reportIds, reportFormatId)
-    print("{numWritten} vulnerabiltity reports written to disk.")
+    print(f"{numWritten} vulnerabiltity reports written to disk.")
 
 # Close the connection once the script is done.
 dbConn.commit()
