56659fcecba787aad38aeed2d1e8b382591c3c9a
Jan Günther <jan.bo.guenther@gmail.com>
2020-10-02 13:06:10 +0200

    remo double update, save time

--- docker/suricata/scripts/update.sh
+++ docker/suricata/scripts/update.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 suricata-update update-sources
-suricata-update
 suricata-update enable-source et/open
 suricata-update enable-source oisf/trafficid
 suricata-update enable-source ptresearch/attackdetection
