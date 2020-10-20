ed96a95c383b009e659f61ee675081500b32befa
Christoph Meyer <christoph.meyer@4sconsult.de>
2020-10-20 08:48:29 +0000

    return to previous steps if exceeding the maximum step

--- docker/web/source/wizard.py
+++ docker/web/source/wizard.py
@@ -1,6 +1,7 @@
-from flask import redirect, Blueprint, render_template
+from flask import redirect, Blueprint, render_template, url_for
 from wtforms_alchemy import ModelForm
 from flask_wtf import FlaskForm
+from source.extensions import db, ma
 
 
 class WizardMiddleware():
@@ -30,6 +31,20 @@ class WizardMiddleware():
         """Evaluate whether the Wizard shall be displayed."""
         return True
 
+    @staticmethod
+    def getMaxStep():
+        """Return the maximum advanced step as endpoint string.
+        
+        For example:
+        Returns 'wizard.systems' if the user has recently completed the box4s step but not yet the systems step.
+        """
+        # WIP CODE:
+        # if System.query.count():
+        if False:
+            return 'wizard.box4s'
+        else:
+            return 'wizard.networks'
+
 
 wizard = Blueprint('wizard', __name__, template_folder='templates/wizard')
 
@@ -46,22 +61,49 @@ def networks():
 
 @wizard.route('/box4s', methods=['GET', 'POST'])
 def box4s():
-    return render_template('box4s.html')
+    endpoint = WizardMiddleware.getMaxStep()
+    if endpoint == 'wizard.box4s':
+        return render_template('box4s.html')
+    else:
+        return redirect(url_for(endpoint))
 
 
 @wizard.route('/systems', methods=['GET', 'POST'])
 def systems():
-    return render_template('systems.html')
+    endpoint = WizardMiddleware.getMaxStep()
+    if endpoint == 'wizard.systems':
+        return render_template('systems.html')
+    else:
+        return redirect(url_for(endpoint))
 
 
 @wizard.route('/mail', methods=['GET', 'POST'])
 def smtp():
-    return render_template('mail.html')
+    endpoint = WizardMiddleware.getMaxStep()
+    if endpoint == 'wizard.smtp':
+        return render_template('mail.html')
+    else:
+        return redirect(url_for(endpoint))
 
 
 @wizard.route('/verify', methods=['GET', 'POST'])
 def verify():
-    return render_template('verify.html')
+    endpoint = WizardMiddleware.getMaxStep()
+    if endpoint == 'wizard.verify':
+        return render_template('verify.html')
+    else:
+        return redirect(url_for(endpoint))
+
+
+# class Network(db.Model):
+#     """Network model class."""
+#     # id = db.Column(db.Integer(), primary_key=True)
+#     # name = db.Column(db.String(50), unique=True)
+#     pass
+
+
+# class System(db.Model):
+#     pass
 
 
 class AddNetworkForm(ModelForm, FlaskForm):
