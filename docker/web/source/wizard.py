7c8b51d6517dff6d1f18bde391df7caec3a5563d
Christoph Meyer <christoph.meyer@4sconsult.de>
2020-10-21 09:42:51 +0000

    System models

--- docker/web/source/wizard.py
+++ docker/web/source/wizard.py
@@ -1,4 +1,4 @@
-from flask import redirect, Blueprint, render_template, url_for
+from flask import redirect, Blueprint, render_template, url_for, request
 from flask.helpers import flash
 from wtforms_alchemy import ModelForm
 from flask_wtf import FlaskForm
@@ -128,6 +128,7 @@ class Network(db.Model):
         3: Scans only when admins and 4s are available
     scan_weekday: lower cased weekday for scans
     scan_time: start time for the scan on `scan_weekday`
+    types: List of associated NetworkTypes
     """
     __tablename__ = 'network'
     id = db.Column(db.Integer(), primary_key=True)
@@ -135,7 +136,7 @@ class Network(db.Model):
     ip_address = db.Column(db.String(24), nullable=False)  # Network Address
     cidr = db.Column(db.Integer(), nullable=False)  # CIDR-Number
     vlan = db.Column(db.String(50))  # VLAN Tag
-    types = db.relationship('Network', secondary='network_networktype')
+    types = db.relationship('NetworkType', secondary='network_networktype')
     scancategory_id = db.Column(db.Integer, db.ForeignKey('scancategory.id'))
     scan_weekday = db.Column(db.String(24))  # lower case
     scan_time = db.Column(db.Time())  # Start time for scan
@@ -174,6 +175,42 @@ class NetworkNetworkType(db.Model):
     id = db.Column(db.Integer(), primary_key=True)
     network_id = db.Column(db.Integer(), db.ForeignKey('network.id', ondelete='CASCADE'))
     networktype_id = db.Column(db.Integer(), db.ForeignKey('networktype.id', ondelete='CASCADE'))
+
+
+class System(db.Model):
+    """System model class.
+
+    Explanation of non-trivial fields:
+    scan_enabled: True if vulnerability scans should be enabled for the system, else False
+    ids_enabled: True if IDS should be enabled for the system, else False
+    types: List of associated SystemTypes
+    """
+    id = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.String(100), unique=True)
+    ip_address = db.Column(db.String(24))  # System IP Address
+    types = db.relationship('SystemType', secondary='system_systemtype')
+    location = db.Column(db.String(255))  # System Location
+    scan_enabled = db.Column(db.Boolean(), default=True)  # Scans active
+    ids_enabled = db.Column(db.Boolean(), default=True)  # IDS enabled
+
+
+class SystemType(db.Model):
+    """Model class for System Types."""
+    __tablename__ = 'systemtype'
+    id = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.String(100))  # System Type Name
+
+    def __repr__(self):
+        """Print SystemType in human readable form."""
+        return '{} ({})'.format(self.name, self.id)
+
+
+class SystemSystemType(db.Model):
+    """Association table for System Types and Systems."""
+    __tablename__ = 'system_systemtype'
+    id = db.Column(db.Integer(), primary_key=True)
+    system_id = db.Column(db.Integer(), db.ForeignKey('system.id', ondelete='CASCADE'))
+    systemtype_id = db.Column(db.Integer(), db.ForeignKey('systemtype.id', ondelete='CASCADE'))
 
 
 class NetworkForm(ModelForm, FlaskForm):
