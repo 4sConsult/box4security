7063be35556a13bd7d7054f006f72935fcc930c6
Christoph Meyer <christoph.meyer@4sconsult.de>
2020-10-22 10:44:53 +0000

    update network and system types

--- docker/web/migrations/versions/b1685fc5f49c_create_types.py
+++ docker/web/migrations/versions/b1685fc5f49c_create_types.py
@@ -8,7 +8,7 @@ Create Date: 2020-10-21 10:01:06.180863
 from alembic import op
 import sqlalchemy as sa
 from sqlalchemy.dialects import postgresql
-from source.wizard import SystemType
+from source.wizard import SystemType, NetworkType
 # revision identifiers, used by Alembic.
 revision = 'b1685fc5f49c'
 down_revision = '9a02836f6117'
@@ -18,9 +18,29 @@ depends_on = None
 
 def upgrade():
     """Upgrade to migration."""
-    op.bulk_insert(SystemType.__table__, [{'name': 'BOX4security'}, ])
+    op.bulk_insert(SystemType.__table__, [
+        {'name': 'BOX4security'},
+        {'name': 'DNS-Server'},
+        {'name': 'Gateway'},
+        {'name': 'Firewall'},
+        {'name': 'IoT'},
+        {'name': 'Industrielle IT'},
+        ])
+    op.bulk_insert(NetworkType.__table__, [
+        {'name': 'Client'},
+        {'name': 'Server'},
+        {'name': 'Gast'},
+    ])
 
 
 def downgrade():
     """Downgrade to migration."""
-    op.execute('DELETE FROM "systemtype" WHERE name="BOX4security"')
+    op.execute(f'DELETE FROM "{SystemType.__table__}" WHERE name="BOX4security"')
+    op.execute(f'DELETE FROM "{SystemType.__table__}" WHERE name="DNS-Server"')
+    op.execute(f'DELETE FROM "{SystemType.__table__}" WHERE name="Gateway"')
+    op.execute(f'DELETE FROM "{SystemType.__table__}" WHERE name="Firewall"')
+    op.execute(f'DELETE FROM "{SystemType.__table__}" WHERE name="IoT"')
+    op.execute(f'DELETE FROM "{SystemType.__table__}" WHERE name="Industrielle IT"')
+    op.execute(f'DELETE FROM "{NetworkType.__table}" WHERE name="Client"')
+    op.execute(f'DELETE FROM "{NetworkType.__table}" WHERE name="Server"')
+    op.execute(f'DELETE FROM "{NetworkType.__table}" WHERE name="Gast"')
