9612881d12f157a5ba0e203d10d7f78e888b898c
Christoph Meyer <christoph.meyer@4sconsult.de>
2020-10-27 12:55:09 +0000

    Add creation of Scan Categories to migration

--- docker/web/migrations/versions/b1685fc5f49c_create_types.py
+++ docker/web/migrations/versions/b1685fc5f49c_create_types.py
@@ -8,7 +8,7 @@ Create Date: 2020-10-21 10:01:06.180863
 from alembic import op
 import sqlalchemy as sa
 from sqlalchemy.dialects import postgresql
-from source.wizard import SystemType, NetworkType
+from source.wizard import SystemType, NetworkType, ScanCategory
 # revision identifiers, used by Alembic.
 revision = 'b1685fc5f49c'
 down_revision = '9a02836f6117'
@@ -31,6 +31,11 @@ def upgrade():
         {'name': 'Server'},
         {'name': 'Gast'},
     ])
+    op.bulk_insert(ScanCategory.__table__, [
+        {'id': 1, 'name': 'Keine Restriktionen bei den Scans'},
+        {'id': 2, 'name': 'Scans ausschließlich zu Randzeiten oder am Wochenende'},
+        {'id': 3, 'name': 'Scans ausschließlich bei Einsatzbereitschaft von 4sConsult und Präsenz der Netzwerkadministration'},
+    ])
 
 
 def downgrade():
@@ -44,3 +49,7 @@ def downgrade():
     op.execute(f'DELETE FROM "{NetworkType.__table}" WHERE name="Client"')
     op.execute(f'DELETE FROM "{NetworkType.__table}" WHERE name="Server"')
     op.execute(f'DELETE FROM "{NetworkType.__table}" WHERE name="Gast"')
+
+    op.execute(f'DELETE FROM "{ScanCategory.__table}" WHERE id="1"')
+    op.execute(f'DELETE FROM "{ScanCategory.__table}" WHERE id="2"')
+    op.execute(f'DELETE FROM "{ScanCategory.__table}" WHERE id="3"')
