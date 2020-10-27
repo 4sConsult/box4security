e75c33a4f9a0df890a6c9239b13c1f5eb24977c0
Christoph Meyer <christoph.meyer@4sconsult.de>
2020-10-27 11:48:09 +0000

    allow nullable dns_id & gateway_id on system table

--- docker/web/migrations/versions/ea1ce32ce8fd_box4s_model.py
+++ docker/web/migrations/versions/ea1ce32ce8fd_box4s_model.py
@@ -18,8 +18,8 @@ depends_on = None
 
 def upgrade():
     """Upgrade to migration."""
-    op.add_column('system', sa.Column('dns_id', sa.Integer(), nullable=False))
-    op.add_column('system', sa.Column('gateway_id', sa.Integer(), nullable=False))
+    op.add_column('system', sa.Column('dns_id', sa.Integer())
+    op.add_column('system', sa.Column('gateway_id', sa.Integer()))
     op.create_foreign_key(None, 'system', 'system', ['gateway_id'], ['id'])
     op.create_foreign_key(None, 'system', 'system', ['dns_id'], ['id'])
 
